c6da6e60c95516f91456229e9f2d11c6ba7cecd9
demich <devit15@yandex.ru>
2020-09-10 15:28:46 +0700

    [nearest] more convenient usage

--- misc/nearest.cpp
+++ misc/nearest.cpp
@@ -1,16 +1,17 @@
 /*
 	vector<int> a;
 	auto next_less = 
-		nearest(a, 1, less<int>());
+		nearest<less>(a, 1);
 	auto prev_greater_or_equal = 
-		nearest(a, -1, greater_equal<int>());
+		nearest<greater_equal>(a, -1);
 */
 
-template<typename T, typename C>
-vector<int> nearest(const vector<T> &v, int dir, const C &cmp){
+template<template<typename> class C, typename T>
+vector<int> nearest(const vector<T> &v, int dir){
+    static constexpr C<T> cmp;
 	assert(abs(dir) == 1);
 	int n = size(v), fin = dir>0 ? n : -1;
-	vector<int> res(n, fin), s;
+	vector<int> s, res(n, fin);
 	for(int i = dir>0 ? 0 : n-1; i!=fin; i+=dir){
 		while(!empty(s) && cmp(v[i],v[s.back()])){
 			res[s.back()] = i;
