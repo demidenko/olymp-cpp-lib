ec8ad71811cfca4ac872ce42d60522d61dbfe5fa
demich <devit15@yandex.ru>
2020-09-13 01:08:39 +0700

    mint: int64 cast instead of multiply

--- numeric/modint.cpp
+++ numeric/modint.cpp
@@ -1,7 +1,7 @@
 int inv_mod(int a, int m){
 	assert(0<a);
 	assert(a<m);
-	return a==1 ? 1 : m-m*1LL*inv_mod(m%a, a)/a;
+	return a==1 ? 1 : m-m*int64_t(inv_mod(m%a, a))/a;
 }
 
 const int mod = 1e9+7 ;
@@ -15,10 +15,10 @@ void operator+=(mint &a, const mint &b){ a.x+=b.x; if(a.x>=mod) a.x-=mod; }
 mint operator+(mint a, const mint &b){ a+=b; return a; }
 void operator-=(mint &a, const mint &b){ a.x-=b.x; if(a.x<0) a.x+=mod; }
 mint operator-(mint a, const mint &b){ a-=b; return a; }
-void operator*=(mint &a, mint b){ a.x = 1LL*a.x*b.x %mod; }
+void operator*=(mint &a, mint b){ a.x = int64_t(a.x)*b.x %mod; }
 mint operator*(mint a, const mint &b){ a*=b; return a; }
-void operator/=(mint &a, const mint &b){ a.x = 1LL*a.x*inv_mod(b.x,mod) %mod; }
+void operator/=(mint &a, const mint &b){ a.x = int64_t(a.x)*inv_mod(b.x,mod) %mod; }
 mint operator/(mint a, const mint &b){ a/=b; return a; }
 mint operator-(mint a){ if(a.x) a.x = mod - a.x; return a; }
-mint operator""m(unsigned long long x){return mint(x);}
+mint operator""m(unsigned long long x){ return mint(x); }
 ostream& operator<<(ostream &o, const mint &m){ return o<<m.x; }
