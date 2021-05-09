747017a4c7de968f2cd3e674c1c71729404fdd8e
demich <devit15@yandex.ru>
2021-05-10 02:13:10 +0700

    =factorizer code golf

--- experemental/factorizer.cpp
+++ experemental/factorizer.cpp
@@ -10,7 +10,7 @@ struct factorizer {
 		}
 	}
 	
-	vector<pair<int,int>> factorize(int x){
+	auto factorize(int x) {
 		vector<pair<int,int>> res;
 		while(x>1){
 			int p = md[x], q = 0;
@@ -20,12 +20,12 @@ struct factorizer {
 		return res;
 	}
 	
-	vector<int> divisors(int x){
+	auto divisors(int x) {
 		static int divs[1500], dn;
 		divs[0] = dn = 1;
-		for(int pr=0,p,k,l; x!=1; x/=p,pr=p){
-			if((p=md[x])!=pr) k=dn, l=0;
-			for(int i=0;i<k;++i) divs[dn++] = divs[l++]*p;
+		for(int p=0,k,l; x!=1; x/=p){
+			if(int c=md[x];c!=p) k=dn, l=0, p=c;
+			for(int i=k;i--;) divs[dn++] = divs[l++]*p;
 		}
 		return vector<int>(divs, divs+dn);
 	}
