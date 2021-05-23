ce18c35a2e1ea481b924e6ca3b96f8176cfc83a3
demich <devit15@yandex.ru>
2021-05-23 21:03:34 +0700

    rsq_add code improved

--- structs/rsq_add.cpp
+++ structs/rsq_add.cpp
@@ -1,32 +1,34 @@
 template<typename T>
 struct rsq_add {
-	vector<pair<T,T>> f;
-	
 	rsq_add(size_t sz): f(sz) {}
 	
-	void add_suf(size_t pos, const T &val){
-		for(size_t i=pos; i<size(f); i|=i+1){
+	void add(size_t l, size_t r, const T &val) {
+		if(l>=r) return ;
+		add_suf(l, val);
+		add_suf(r, -val);
+	}
+	
+	T operator()(size_t l, size_t r) {
+		if(l>=r) return T();
+		return sum_until(r) - sum_until(l);
+	}
+	
+	T operator[](size_t i){ return operator()(i,i+1); }
+	
+	private:
+	vector<pair<T,T>> f;
+	
+	void add_suf(size_t pos, const T &val) {
+		for(size_t i=pos; i<size(f); i|=i+1) {
 			f[i].first+=val;
 			f[i].second+=val*pos;
 		}
 	}
 	
-	void add(size_t l, size_t r, const T &val){
-		add_suf(l, val);
-		add_suf(r, -val);
-	}
-	
-	T sum_pref(int pos){
+	T sum_until(size_t pos) {
 		T res = T();
-		for(int i=pos; i>=0; i=(i&(i+1))-1) 
-			res += f[i].first*(pos+1) - f[i].second;
+		for(size_t i=pos; i-->0; i&=i+1) 
+			res += f[i].first*pos - f[i].second;
 		return res;
 	}
-	
-	T operator()(int l, int r){
-		if(l>=r) return T();
-		return sum_pref(r-1) - sum_pref(l-1);
-	}
-	
-	T operator[](int i){ return operator()(i,i+1); }
 };
