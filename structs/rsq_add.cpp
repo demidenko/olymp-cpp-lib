00f179f1450d3c54d763f270812bcdf17c57fe04
demich <devit15@yandex.ru>
2021-05-23 21:46:57 +0700

    rsq_add casts improve

--- structs/rsq_add.cpp
+++ structs/rsq_add.cpp
@@ -4,10 +4,9 @@ struct rsq_add {
 	
 	rsq_add(const vector<auto> &vals): f(size(vals)) {
         for(size_t i=0; i<size(vals); ++i) {
-            T x = T(vals[i]) - (i ? T(vals[i-1]) : T());
-            T y = x*T(i);
+            T x = i ? T(vals[i]) - T(vals[i-1]) : vals[i];
             f[i].first+=x;
-            f[i].second+=y;
+            f[i].second+=x*T(i);
             if(size_t j = i|(i+1); j<size(f)){
                 f[j].first+=f[i].first;
                 f[j].second+=f[i].second;
@@ -34,14 +33,14 @@ struct rsq_add {
 	void add_suf(size_t pos, const T &val) {
 		for(size_t i=pos; i<size(f); i|=i+1) {
 			f[i].first+=val;
-			f[i].second+=val*pos;
+			f[i].second+=val*T(pos);
 		}
 	}
 	
 	T sum_until(size_t pos) {
 		T res = T();
 		for(size_t i=pos; i-->0; i&=i+1) 
-			res += f[i].first*pos - f[i].second;
+			res += f[i].first*T(pos) - f[i].second;
 		return res;
 	}
 };
