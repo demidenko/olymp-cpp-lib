23d36a2aa7a2c30272c1c7d6ae7cfb2bcc200f16
demich <devit15@yandex.ru>
2021-05-25 01:36:29 +0700

    Cnk size_t

--- numeric/modint.cpp
+++ numeric/modint.cpp
@@ -34,14 +34,14 @@ mint mpow(mint a, uint64_t n){
 
 struct Cnk {
 	vector<mint> f, fi;
-	Cnk(int n): f(n+1), fi(n+1){
+	Cnk(size_t n): f(n+1), fi(n+1){
 		f[0] = fi[0] = 1;
-		for(int i=1; i<=n; ++i) f[i] = f[i-1]*i;
+		for(size_t i=1; i<=n; ++i) f[i] = f[i-1]*i;
 		fi[n] = 1 / f[n];
-		for(int i=n; i>1; --i) fi[i-1] = fi[i]*i;
+		for(size_t i=n; i>1; --i) fi[i-1] = fi[i]*i;
 	}
-	mint operator()(int n, int k){
-		if(n<k) return 0; assert(k>=0 && n<size(f));
+	mint operator()(size_t n, size_t k){
+		if(n<k) return 0; assert(n<size(f));
 		return f[n]*fi[n-k]*fi[k];
 	}
 };
