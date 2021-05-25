a31b455eaf9f8fa805662d8508441ffbda266bde
demich <devit15@yandex.ru>
2021-05-25 16:25:59 +0700

    segt improved

--- experemental/segt.cpp
+++ experemental/segt.cpp
@@ -1,14 +1,11 @@
-template<typename T, T(*f)(const T&, const T&), T (*get_neutral)() >
+template<typename T, T(*f)(const T&, const T&)>
 struct segt {
 	
-	vector<T> t;
-	size_t d;
-	
-	segt(size_t n, function<T(size_t)> get) {
+	segt(size_t n, function<T(size_t)> gen) {
 		for(d=1; d<n; d<<=1);
-		t.assign(d*2, get_neutral());
-		for(size_t i=0;i<n;++i) t[i+d] = get(i);
-		for(int i=d-1;i>0;--i) t[i] = f(t[i*2], t[i*2+1]);
+		t.assign(d*2, T());
+		for(size_t i=0;i<n;++i) t[i+d] = gen(i);
+		for(size_t i=d;i-->1;) t[i] = f(t[i*2], t[i*2+1]);
 	}
 	
 	void set_value(size_t i, const T& val){
@@ -17,7 +14,7 @@ struct segt {
 	}
 	
 	T operator()(size_t l, size_t r){
-		const static T neutral = get_neutral();
+		const static T neutral = T();
 		T fl = neutral, fr = neutral;
 		for(l+=d,r+=d; l<r; l>>=1,r>>=1){
 			if(l&1) fl = f(fl, t[l]), ++l;
@@ -26,5 +23,9 @@ struct segt {
 		return f(fl,fr);
 	}
 	
-	T operator()(){ return t[1]; }
+	const T& operator()(){ return t[1]; }
+	
+	private:
+	vector<T> t;
+	size_t d;
 };
