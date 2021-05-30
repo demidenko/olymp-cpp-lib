71bacd168890c269b2ab9da82d76781fc71a01bd
demich <devit15@yandex.ru>
2021-05-31 05:39:59 +0700

    binary lifting improved

--- misc/binary_lifter.cpp
+++ misc/binary_lifter.cpp
@@ -1,30 +1,32 @@
 struct binary_lifter {
-	binary_lifter(const vector<int> &p) {
-		int n = size(p);
-		for(int i=0;i<n;++i) assert(p[i]==-1 || (0<=p[i] && p[i]<n && p[i]!=i)); //weak check
+	binary_lifter(const vector<size_t> &p): n(size(p)) {
+		for(size_t i=0;i<n;++i) assert(0<=p[i] && p[i]<=n && p[i]!=i); //weak check
 		for(L = 1; (1<<L)<n; ++L);
-		jump.assign(L,vector<int>(n,-1));
-		jump[0] = p;
-		for(int h=1;h<L;++h){
-			for(int i=0;i<n;++i){
-				int j = jump[h-1][i];
-				jump[h][i] = (j == -1 ? -1 : jump[h-1][j]);
-			}
+		jump.assign(L, vector(n+1,n));
+		copy(ALL(p), begin(jump[0]));
+		jump[0][n] = n;
+		for(size_t h=1;h<L;++h){
+			for(size_t i=0;i<=n;++i) jump[h][i] = jump[h-1][jump[h-1][i]];
 		}
 	}
 	
-	//returns <first fail, count of jumps, last nonfail>
-	tuple<int,int,int> first_fail(int i, function<bool(int)> &&is_fail) {
-		if(is_fail(i)) return { i, 0, -1 };
-		int jumps = 0;
-		for(int h=L-1;h>=0;--h){
-			int j = jump[h][i];
-			if(j!=-1 && !is_fail(j)) i = j, jumps+=1<<h;
+	struct result { size_t first_fail, count_of_jumps, last_nonfail; };
+	result first_fail(size_t i, const function<bool(size_t)> &is_fail) {
+		if(is_fail(i)) return { i, 0, n };
+		size_t jumps = 0;
+		for(size_t h=L; h--;){
+			size_t j = jump[h][i];
+			if(j<n && !is_fail(j)) i = j, jumps+=1<<h;
 		}
 		return { jump[0][i], jumps+1, i };
 	}
 	
+	size_t kth_jump(size_t i, size_t k) {
+		for(size_t l=0;l<L;++l) if(k>>l&1) i = jump[l][i];
+		return i;
+	}
+	
 	private:
-	vector<vector<int>> jump;
-	int L;
+	size_t n, L;
+	vector<vector<size_t>> jump;
 };
