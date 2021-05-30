bb10ce5823f2b350b22227bd066cdf270eafe9a1
demich <devit15@yandex.ru>
2021-05-30 18:33:21 +0700

    fix for forests

--- alpha/tree_dp_root_each.cpp
+++ alpha/tree_dp_root_each.cpp
@@ -4,19 +4,8 @@ vector<T> tree_dp_root_each(
 	function<T(size_t)> init,
 	function<T(T, T)> f
 ) {
-	size_t n = size(g);
-	vector<T> result(n), sub(n);
-	
-	function<void(size_t,size_t)> go_sub = [&](size_t v, size_t p) {
-		sub[v] = init(v);
-		for(size_t i : g[v]) if(i!=p) {
-			go_sub(i, v);
-			sub[v] = f(sub[v], sub[i]);
-		}
-	};
-	
 	using it = typename vector<T>::iterator;
-	function<void(it,it,T)> exclude = [&](it l, it r, const T& val) {
+	function<void(it,it,T)> exclude = [&f,&exclude](it l, it r, const T& val) {
 		if(l==r) return ;
 		if(l+1 == r) { *l = val; return ; }
 		auto m = l + (r-l)/2;
@@ -26,22 +15,37 @@ vector<T> tree_dp_root_each(
 		exclude(m, r, fl);
 	};
 	
+	size_t n = size(g);
+	vector<T> result(n), subtree(n);
+	vector<bool> used(n);
+	
+	function<void(size_t)> go_sub = [&](size_t v) {
+		used[v] = true;
+		subtree[v] = init(v);
+		for(size_t i : g[v]) if(!used[i]) {
+			go_sub(i);
+			subtree[v] = f(subtree[v], subtree[i]);
+		}
+	};
+	
 	function<void(size_t,size_t,T)> go = [&](size_t v, size_t p, T up) {
+		used[v] = true;
 		T dv = init(v);
 		result[v] = p==v ? dv : f(dv, up);
 		vector<T> a;
-		for(size_t i : g[v]) if(i!=p) a.push_back(sub[i]);
+		for(size_t i : g[v]) if(i!=p) a.push_back(subtree[i]);
 		exclude(ALL(a), dv);
 		auto it = begin(a);
 		for(size_t i : g[v]) if(i!=p) {
 			go(i, v, p==v ? *it : f(*it,up));
-			result[v] = f(result[v], sub[i]);
 			++it;
+			result[v] = f(result[v], subtree[i]);
 		}
 	};
 	
-	go_sub(0,0);
-	go(0,0,{});
+	for(size_t i=0;i<n;++i) if(!used[i]) go_sub(i);
+	used.flip();
+	for(size_t i=0;i<n;++i) if(!used[i]) go(i,i,{});
 	
 	return result;
 }
