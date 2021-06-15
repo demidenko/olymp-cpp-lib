1549fb927459f5e58e88bd070c8b3793a3912e28
demich <devit15@yandex.ru>
2021-06-15 10:49:44 +0700

    segment tree: constructor instead of merge function

--- structs/segment_tree.cpp
+++ structs/segment_tree.cpp
@@ -1,25 +1,25 @@
-template<typename T, T f(const T&, const T&), bool well_formed = false>
+template<typename T, bool well_formed = false>
 struct segment_tree {
 	segment_tree(size_t n, const function<T(size_t)> &gen) {
-		if constexpr (!well_formed) d = n;
+		if constexpr(!well_formed) d = n;
 		else for(d=1; d<n; d<<=1);
 		t.assign(d*2, T());
 		for(size_t i=0;i<n;++i) t[i+d] = gen(i);
-		for(size_t i=d;i-->1;) t[i] = f(t[i*2], t[i*2+1]);
+		for(size_t i=d;i-->1;) t[i] = T(t[i*2], t[i*2+1]);
 	}
 	
 	void set_value(size_t i, const T &value) {
-		for(t[i+=d] = value; i>>=1; ) t[i] = f(t[i*2], t[i*2+1]);
+		for(t[i+=d] = value; i>>=1; ) t[i] = T(t[i*2], t[i*2+1]);
 	}
 	
 	T operator()(size_t l, size_t r) const {
 		const static T neutral = T();
 		T fl = neutral, fr = neutral;
 		for(l+=d,r+=d; l<r; l>>=1,r>>=1){
-			if(l&1) fl = f(fl, t[l]), ++l;
-			if(r&1) --r, fr = f(t[r], fr);
+			if(l&1) fl = T(fl, t[l]), ++l;
+			if(r&1) --r, fr = T(t[r], fr);
 		}
-		return f(fl,fr);
+		return T(fl,fr);
 	}
 	
 	decltype(auto) operator()() const {
