04158689e2dee7f0f8b0d90cf61f27e1830e820f
demich <devit15@yandex.ru>
2021-06-16 10:49:25 +0700

    kth_jump: overjump prevent

--- misc/binary_lifter.cpp
+++ misc/binary_lifter.cpp
@@ -1,13 +1,12 @@
 struct binary_lifter {
 	constexpr static inline size_t none = -1;
 	
-	binary_lifter(): L(1), jump(1) {}
+	binary_lifter() { init(0); }
 	
 	binary_lifter(const vector<size_t> &p) {
 		size_t n = size(p);
 		for(size_t i=0;i<n;++i) assert(p[i] == none || (0<=p[i] && p[i]<n && p[i]!=i)); //weak check
-		for(L = 1; (1<<L)<n; ++L);
-		jump.assign(L, vector(n,none));
+		init(n);
 		jump[0] = p;
 		for(size_t h=1; h<L; ++h)
 			for(size_t i=0; i<n; ++i)
@@ -44,8 +43,9 @@ struct binary_lifter {
 	}
 	
 	size_t kth_jump(size_t i, size_t k) const {
+		if(k >= size_t(1)<<L) return none;
 		for(size_t h=0; k>0 && i!=none; ++h, k>>=1) if(k&1) i = jump[h][i];
-		return i;
+		return i; 
 	}
 	
 	size_t count() const { return size(jump[0]); }
@@ -53,4 +53,9 @@ struct binary_lifter {
 	private:
 	size_t L;
 	vector<vector<size_t>> jump;
+	
+	void init(size_t n) {
+		for(L = 1; size_t(1)<<L < n; ++L);
+		jump.assign(L, vector(n,none));
+	}
 };
