6458cb2fb58b60b21a8998e23d2908b113a0e023
demich <devit15@yandex.ru>
2021-06-18 17:50:25 +0700

    graph usage improved

--- graphs/graph.cpp
+++ graphs/graph.cpp
@@ -1,6 +1,12 @@
-template<typename ...T>
-struct graph_t {
-	using E = conditional_t<sizeof...(T), tuple<size_t,T...>, size_t>;
+template<class ...T> struct edge_t : public tuple<size_t,T...> {
+	edge_t(const auto&...args): tuple<size_t,T...>(args...) { }
+	operator size_t() const { return get<0>(*this); }
+};
+template<class...T> struct std::tuple_size<edge_t<T...>> : tuple_size<tuple<size_t,T...>> {};
+template<size_t I, class...T> struct std::tuple_element<I,edge_t<T...>> : tuple_element<I,tuple<size_t,T...>> {};
+
+template<class ...T> struct graph_t {
+	using E = conditional_t<sizeof...(T), edge_t<T...>, size_t>;
 	graph_t(size_t n = 0): g(n) {}
 	void add_dir_edge(size_t from, size_t to, const T&... args) {
 		assert(from<size() && to<size());
