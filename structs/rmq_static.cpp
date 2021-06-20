20b2ca64e6025c8f401608ae1f38569b39c28fdf
demich <devit15@yandex.ru>
2021-06-20 20:41:46 +0700

    log2/pow2 stuff fixed

--- structs/rmq_static.cpp
+++ structs/rmq_static.cpp
@@ -1,9 +1,8 @@
-template<typename T, const T& f(const T&, const T&) = std::min<T> >
+template<class T, const T& f(const T&, const T&) = std::min<T> >
 struct rmq {
 	rmq() {}
 	rmq(const vector<T> &vals) {
-		size_t n = size(vals), L = 1;
-		while((1<<L) <= n) ++L;
+		size_t n = size(vals), L = n ? __lg(n) + 1 : 1;
 		t.resize(L); 
 		t[0] = vals;
 		for(size_t p=1, k=1; k<L; ++k, p<<=1){
@@ -13,8 +12,8 @@ struct rmq {
 		}
 	}
 	const T& operator()(size_t l, size_t r) const {
-		int h = 31 - __builtin_clz(r-l);
-		return f(t[h][l], t[h][r-(1<<h)]);
+		auto h = __lg(r-l);
+		return f(t[h][l], t[h][r-(size_t(1)<<h)]);
 	}
 	private: vector<vector<T>> t;
 };
