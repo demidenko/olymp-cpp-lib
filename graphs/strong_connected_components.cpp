d519650a35a75cee5f2a7535b6f6b6ae18f9a3d1
demich <devit15@yandex.ru>
2021-06-24 20:45:47 +0700

    scc return count

--- graphs/strong_connected_components.cpp
+++ graphs/strong_connected_components.cpp
@@ -1,10 +1,10 @@
-vector<size_t> strong_connected_components(const graph &g) {
+auto strong_connected_components(const auto &g) {
 	size_t n = size(g), tn = 0, cn = 0;
 	vector<size_t> tin(n), h(n), comp(n), s;
 	function<void(size_t)> css = [&](size_t v) {
 		h[v] = tin[v] = ++tn;
 		s.push_back(v);
-		for(size_t i : g[v]){
+		for(size_t i : g[v]) {
 			if(!tin[i]) css(i);
 			if(tin[i] && !comp[i]) h[v] = min(h[v], h[i]);
 		}
@@ -12,5 +12,5 @@ vector<size_t> strong_connected_components(const graph &g) {
 	};
 	for(size_t i=0; i<n; ++i) if(!tin[i]) css(i);
 	for(size_t &x : comp) x = cn-x;
-	return comp;
+	return pair(cn, comp);
 };
