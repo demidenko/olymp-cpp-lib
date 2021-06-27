c58d34648ffa4f42101a5560e85b13c64554aad5
demich <devit15@yandex.ru>
2021-06-28 03:03:33 +0700

    dijkstra remember from

--- beta/dijkstra.cpp
+++ beta/dijkstra.cpp
@@ -1,25 +1,28 @@
-template<typename S, typename T>
+template<class S, class T>
 auto dijkstra(const S &start,
-    function<void(const S&, const function<void(const S&, const T&)>&)> gen
-	//pass as [](const auto &s, auto &upd){... upd(to, edge_cost); ...}
-){
-    map<S, T> dist{{start,0}};
-    set<pair<T,S>> q{{0,start}};
-    T d;
-    auto upd = [&dist, &q, &d](const S &v, const T &edge_cost) {
-        const T cost = edge_cost + d;
-        if(auto it = dist.find(v); it!=end(dist)) {
-            if(T& cur = it->second; cost < cur) {
-                auto e = q.extract({cur,v});
-                e.value().first = cur = cost;
-                q.insert(move(e));
-            }
-        } else q.emplace(dist[v] = cost, v);
-    };
-    while(!empty(q)) {
-        auto p = move(q.extract(begin(q)).value());
-        d = p.first;
-        gen(p.second, upd);
-    }
-    return dist;
+	function<void(const S&, function<void(const S&, T)>)> gen
+	//pass as [](const auto &v, auto upd){... upd(to, edge_cost); ...}
+) {
+	map<S, pair<T,S>> dist{{start,{0,start}}};
+	set<pair<T,S>> q{{0,start}};
+	T dv; S v;
+	auto upd = [&dist, &q, &dv, &v](const S &t, T edge_cost) {
+		const T cur = dv + edge_cost;
+		if(auto it = dist.find(t); it!=end(dist)) {
+			if(auto &[dt, how] = it->second; cur < dt) {
+				auto e = q.extract({dt,t});
+				e.value().first = dt = cur;
+				how = v;
+				q.insert(move(e));
+			}
+		} else {
+			dist[t] = {cur, v};
+			q.emplace(cur, t);
+		}
+	};
+	while(!empty(q)) {
+        tie(dv, v) = move(q.extract(begin(q)).value());
+		gen(v, upd);
+	}
+	return dist;
 };
