3e6254d35c4d3c992bd38d53d68bea557635eedb
demich <devit15@yandex.ru>
2021-06-29 10:02:49 +0700

    dsu code update

--- graphs/dsu.cpp
+++ graphs/dsu.cpp
@@ -1,14 +1,12 @@
 struct dsu {
-	dsu(size_t n = 0): sz(n,1), p(n) {
-		iota(begin(p),end(p),0);
-	}
-	size_t get(size_t i){
+	explicit dsu(size_t n = 0): sz(n,1), p(n) { iota(begin(p),end(p),0); }
+	size_t get(size_t i) {
 		size_t v = i;
 		while(v!=p[v]) v = p[v];
 		while(i!=p[i]) i = exchange(p[i],v);
 		return v;
 	}
-	bool unite(size_t i, size_t j){
+	bool unite(size_t i, size_t j) {
 		i = get(i);
 		j = get(j);
 		if(i==j) return false;
@@ -17,10 +15,7 @@ struct dsu {
 		sz[i]+=sz[j];
 		return true;
 	}
-	size_t size(size_t i){
-		return sz[get(i)];
-	}
-	
-	private:
-	vector<size_t> p, sz;
+	size_t size(size_t i) { return sz[get(i)]; }
+	size_t operator[](size_t i) { return get(i); }
+	private: vector<size_t> p, sz;
 };
