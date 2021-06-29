29f7ad58f6694827ae9a8cb2e79133d861c5b315
demich <devit15@yandex.ru>
2021-06-29 09:55:00 +0700

    rsq code update

--- structs/rsq.cpp
+++ structs/rsq.cpp
@@ -1,15 +1,12 @@
-template<typename T>
+template<class T>
 struct rsq {
-	rsq(size_t sz = 0): f(sz) {}
-	rsq(const vector<auto> &vals): f(size(vals)) {
-		for(auto i = size(vals); i--;) {
-			f[i]+=vals[i];
-			if(auto j = i&(i+1); j) f[j-1]+=f[i];
-		}
+	explicit rsq(size_t sz = 0): f(sz) {}
+	rsq(const vector<auto> &vals): f(begin(vals),end(vals)) {
+		for(auto i = size(vals); i--;) if(auto j = i&(i+1); j) f[j-1]+=f[i];
 	}
 	T sum_suf(size_t i) const {
-		T s = T();
-		for(;i<size(f);i|=i+1) s+=f[i];
+		T s{};
+		for(; i<size(f); i|=i+1) s+=f[i];
 		return s;
 	}
 	T operator()(size_t l, size_t r) const {
@@ -17,7 +14,7 @@ struct rsq {
 		return sum_suf(l) - sum_suf(r);
 	}
 	void add(size_t i, const T &val) {
-		for(++i; i; i&=i+1) f[--i]+=val;
+		for(++i; i--; i&=i+1) f[i]+=val;
 	}
 	private: vector<T> f;
 };
