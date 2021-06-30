b46a2c25bbe9bc51847ffa216184d31362cc22e4
demich <devit15@yandex.ru>
2021-07-01 02:50:27 +0700

    graph_t code update

--- graphs/graph.cpp
+++ graphs/graph.cpp
@@ -1,5 +1,5 @@
 template<class ...T> struct edge_t : public tuple<size_t,T...> {
-	edge_t(const auto&...args): tuple<size_t,T...>(args...) { }
+	edge_t(auto&&... args): tuple<size_t,T...>(args...) { }
 	operator size_t() const { return get<0>(*this); }
 };
 template<class...T> struct std::tuple_size<edge_t<T...>> : tuple_size<tuple<size_t,T...>> {};
@@ -7,12 +7,12 @@ template<size_t I, class...T> struct std::tuple_element<I,edge_t<T...>> : tuple_
 
 template<class ...T> struct graph_t {
 	using E = conditional_t<sizeof...(T), edge_t<T...>, size_t>;
-	graph_t(size_t n = 0): g(n) {}
-	void add_dir_edge(size_t from, size_t to, const T&... args) {
+	explicit graph_t(size_t n): g(n) {}
+	void add_dir_edge(size_t from, size_t to, auto&&... args) {
 		assert(from<size() && to<size());
 		g[from].emplace_back(to, args...);
 	}
-	void add_edge(size_t x, size_t y, const T&... args) {
+	void add_edge(size_t x, size_t y, auto&&... args) {
 		add_dir_edge(x, y, args...);
 		add_dir_edge(y, x, args...);
 	}
