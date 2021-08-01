f9b835ab1b982c4f49b816001b7092e67afc9dfb
demich <devit15@yandex.ru>
2021-08-01 17:30:28 +0700

    segt range operation improved

--- beta/segment_tree_range_operation.cpp
+++ beta/segment_tree_range_operation.cpp
@@ -24,7 +24,7 @@ struct segment_tree {
 	vector<pair<T,optional<O>>> t;
 	size_t d;
 	
-	void fall(size_t v, size_t length, const O &operation) {
+	void cover(size_t v, const O &operation, size_t length) {
 		t[v].first = T(t[v].first, operation, length);
 		if(auto &op = t[v].second) op = O(*op, operation);
 		else op = operation;
@@ -32,21 +32,26 @@ struct segment_tree {
 	
 	void push(size_t v, size_t length) {
 		if(auto &op = t[v].second) {
-			fall(v*2, length/2, *op);
-			fall(v*2+1, length/2, *op);
+			auto [opl, opr] = op->split(length/2, length/2);
+			cover(v*2, opl, length/2);
+			cover(v*2+1, opr, length/2);
 			op.reset();
 		}
 	}
 	
 	void apply(size_t i, size_t j, const O &operation, size_t l, size_t r, size_t v) {
 		if(i==l && j==r) {
-			fall(v, r-l, operation);
+			cover(v, operation, r-l);
 			return ;
 		}
 		push(v, r-l);
 		size_t m = (l+r)>>1;
-		if(i<m) apply(i, min(j,m), operation, l, m, v*2);
-		if(m<j) apply(max(i,m), j, operation, m, r, v*2+1);
+		if(j<=m) apply(i, j, operation, l, m, v*2); else
+		if(i>=m) apply(i, j, operation, m, r, v*2+1); else {
+			auto [opl, opr] = operation.split(m-i, j-m);
+			apply(i, m, opl, l, m, v*2);
+			apply(m, j, opr, m, r, v*2+1);
+		}
 		t[v].first = T(t[v*2].first, t[v*2+1].first);
 	}
 	
@@ -63,6 +68,7 @@ struct segment_tree {
 implement:
 struct operation {
 	operation(const operation &first, const operation &second)
+	pair<operation, operation> split(size_t pref, size_t suf) const { return {*this, *this}; }
 };
 struct node {
 	node()
