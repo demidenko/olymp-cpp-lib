d41a093e4c9ae1eb149584d92aab8630fcbed3b7
demich <devit15@yandex.ru>
2021-08-02 09:35:59 +0700

    code short

--- beta/mitm_table.cpp
+++ beta/mitm_table.cpp
@@ -3,15 +3,12 @@ struct mitm_table {
 	mitm_table(const vector<auto> &vals) {
 		size_t n = size(vals), L = n ? __lg(n) + 1 : 1;
 		size_t d = size_t(1)<<L;
-		
 		t.resize(L);
 		auto &v = t[0];
 		v.assign(d, neutral);
 		copy(begin(vals), end(vals), begin(v));
-		
 		for(size_t p=4, k=1; k<L; ++k, p<<=1) {
-			auto &a = t[k];
-			a.assign(begin(v), end(v));
+			auto &a = t[k] = v;
 			for(size_t l = 0; l<d; l+=p) {
 				size_t mid = l+(p>>1);
 				for(size_t i=mid-1; i>l; --i) a[i-1] = T(a[i-1], a[i]);
