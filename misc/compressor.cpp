58857ab95d24d9f82274b7ea03896ca1ef91f4f3
demich <devit15@yandex.ru>
2021-08-05 08:53:54 +0700

    compressor search type fixes

--- misc/compressor.cpp
+++ misc/compressor.cpp
@@ -7,14 +7,14 @@ struct compressor {
 		v.resize(unique(begin(v),end(v))-begin(v));
 	}
 	void add(const T &x) { v.push_back(x); }
-	size_t lb(const T &x) const { return lower_bound(begin(v),end(v),x) - begin(v); }
-	size_t ub(const T &x) const { return upper_bound(begin(v),end(v),x) - begin(v); }
-	size_t index(const T &x) const {
+	size_t lb(auto &&x) const { return lower_bound(begin(v),end(v),x) - begin(v); }
+	size_t ub(auto &&x) const { return upper_bound(begin(v),end(v),x) - begin(v); }
+	size_t index(auto &&x) const {
 		size_t i = lb(x);
 		assert(v.at(i) == x);
 		return i;
 	}
-	auto range(const T &from, const T &to) const { return pair(lb(from),lb(to)); }
+	auto range(auto &&__begin, auto &&__end) const { return pair(lb(__begin),lb(__end)); }
 	size_t size() const { return v.size(); }
 	private: vector<T> v;
 };
