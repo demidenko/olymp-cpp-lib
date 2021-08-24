e4bf48dee1ab8324bb5e75c685b700532613a315
demich <devit15@yandex.ru>
2021-08-24 13:15:38 +0700

    segment tree memory improve

--- beta/segment_tree_range_operation.cpp
+++ beta/segment_tree_range_operation.cpp
@@ -1,10 +1,7 @@
 template<class T, class O>
 struct segment_tree {
-	segment_tree(size_t n, function<T(size_t)> gen) {
-		for(d=1; d<n; d<<=1);
-		t.assign(d*2, {{}, nullopt});
-		for(size_t i=0;i<n;++i) t[i+d].first = gen(i);
-		for(size_t i=d;i-->1;) t[i].first = T(t[i*2].first, t[i*2+1].first);
+	segment_tree(size_t n, function<T(size_t)> gen): d(n), t(d*2, {{}, nullopt}) {
+		if(n) build(0, d, n=1, gen);
 	}
 	
 	void apply(size_t l, size_t r, const O &operation) {
@@ -21,8 +18,8 @@ struct segment_tree {
 	const T& operator()() const { return t[1].first; }
 	
 	private:
+	const size_t d;
 	vector<pair<T,optional<O>>> t;
-	size_t d;
 	
 	void cover(size_t v, const O &operation, size_t length) {
 		t[v].first = T(t[v].first, operation, length);
@@ -32,9 +29,10 @@ struct segment_tree {
 	
 	void push(size_t v, size_t length) {
 		if(auto &op = t[v].second) {
-			auto [opl, opr] = op->split(length/2, length/2);
-			cover(v*2, opl, length/2);
-			cover(v*2+1, opr, length/2);
+			size_t sl = length / 2;
+			auto [opl, opr] = op->split(sl, length - sl);
+			cover(v+1, opl, sl);
+			cover(v+sl*2, opr, length - sl);
 			op.reset();
 		}
 	}
@@ -45,34 +43,31 @@ struct segment_tree {
 			return ;
 		}
 		push(v, r-l);
-		size_t m = (l+r)>>1;
-		if(j<=m) apply(i, j, operation, l, m, v*2); else
-		if(i>=m) apply(i, j, operation, m, r, v*2+1); else {
+		size_t m = (l+r)>>1, vr = v + (m-l)*2;
+		if(j<=m) apply(i, j, operation, l, m, v+1); else
+		if(i>=m) apply(i, j, operation, m, r, vr); else {
 			auto [opl, opr] = operation.split(m-i, j-m);
-			apply(i, m, opl, l, m, v*2);
-			apply(m, j, opr, m, r, v*2+1);
+			apply(i, m, opl, l, m, v+1);
+			apply(m, j, opr, m, r, vr);
 		}
-		t[v].first = T(t[v*2].first, t[v*2+1].first);
+		t[v].first = T(t[v+1].first, t[vr].first);
 	}
 	
 	T query(size_t i, size_t j, size_t l, size_t r, size_t v) {
 		if(i==l && j==r) return t[v].first;
 		push(v, r-l);
-		size_t m = (l+r)>>1;
-		if(j<=m) return query(i, j, l, m, v*2);
-		if(i>=m) return query(i, j, m, r, v*2+1);
-		return T(query(i,m,l,m,v*2), query(m,j,m,r,v*2+1));
+		size_t m = (l+r)>>1, vr = v + (m-l)*2;
+		if(j<=m) return query(i, j, l, m, v+1);
+		if(i>=m) return query(i, j, m, r, vr);
+		return T(query(i,m,l,m,v+1), query(m,j,m,r,vr));
+	}
+	
+	size_t build(size_t l, size_t r, size_t &v, auto gen) {
+		size_t cur = v++;
+		if(l+1 == r) t[cur].first = gen(l); else {
+			size_t m = (l+r) >> 1, vl = build(l, m, v, gen), vr = build(m, r, v, gen);
+			t[cur].first = T(t[vl].first, t[vr].first);
+		}
+		return cur;
 	}
 };
-/*
-implement:
-struct operation {
-	operation(const operation &first, const operation &second)
-	pair<operation, operation> split(size_t pref, size_t suf) const { return {*this, *this}; }
-};
-struct node {
-	node()
-	node(const node &l, const node &r)
-	node(const node &v, const operation &o, size_t len)
-};
-*/
