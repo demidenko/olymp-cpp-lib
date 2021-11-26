f05cc8953682ca61fdd505817806fdf6d18021b7
demich <devit15@yandex.ru>
2021-11-26 18:11:23 +0700

    code upd

--- numeric/binomial.cpp
+++ numeric/binomial.cpp
@@ -1,13 +1,13 @@
-struct binomial {
-	vector<mint> f, fi;
-	binomial(size_t n): f(n+1), fi(n+1) {
-		f[0] = fi[0] = 1;
-		for(size_t i=1; i<=n; ++i) f[i] = f[i-1]*i;
-		fi[n] = 1 / f[n];
-		for(size_t i=n; i>1; --i) fi[i-1] = fi[i]*i;
+template<class T = mint> struct binomial {
+	vector<T> f, finv;
+	binomial(size_t n): f(n+1), finv(n+1) {
+		f[0] = finv[0] = 1;
+		for(size_t i=1; i<=n; ++i) f[i] = f[i-1] * i;
+		finv[n] = 1 / f[n];
+		for(size_t i=n; i>1; --i) finv[i-1] = finv[i] * i;
 	}
-	mint operator()(size_t n, size_t k) {
-		if(n<k) return 0; assert(n<size(f));
-		return f[n]*fi[n-k]*fi[k];
+	T operator()(size_t n, size_t k) {
+		if(n<k) return 0;
+		return f.at(n) * finv[n-k] * finv[k];
 	}
 };
--- numeric/modint.cpp
+++ numeric/modint.cpp
@@ -1,28 +1,26 @@
-int inv_mod(int a, int m){
-	assert(0<a);
-	assert(a<m);
-	return a==1 ? 1 : m-m*int64_t(inv_mod(m%a, a))/a;
+int inv_mod(int a, int m) { assert(0<a); assert(a<m);
+	return a==1 ? 1 : m - (int64_t)m * inv_mod(m%a, a) / a;
 }
 
 template<int mod> struct modint {
 	modint(): x(0) {}
-	modint(const auto &val): x(val%mod) { if(x<0) x+=mod; }
+	modint(auto &&val): x(val%mod) { if(x<0) x+=mod; }
 	static int get_mod() { return mod; }
-	void operator+=(const modint &b){ x+=b.x; if(x>=mod) x-=mod; }
-	void operator-=(const modint &b){ x-=b.x; if(x<0) x+=mod; }
-	void operator*=(const modint &b){ x = int64_t(x)*b.x %mod; }
-	void operator/=(const modint &b){ x = int64_t(x)*inv_mod(b.x,mod) %mod; }
-	friend modint operator+(modint a, const modint &b){ a+=b; return a; }
-	friend modint operator-(modint a, const modint &b){ a-=b; return a; }
-	friend modint operator*(modint a, const modint &b){ a*=b; return a; }
-	friend modint operator/(modint a, const modint &b){ a/=b; return a; }
-	friend modint operator-(modint a){ if(a.x) a.x = mod - a.x; return a; }
+	void operator+=(const modint &b) { x+=b.x; if(x>=mod) x-=mod; }
+	void operator-=(const modint &b) { x-=b.x; if(x<0) x+=mod; }
+	void operator*=(const modint &b) { x = int64_t(x)*b.x %mod; }
+	void operator/=(const modint &b) { x = int64_t(x)*inv_mod(b.x,mod) %mod; }
+	friend modint operator+(modint a, const modint &b) { a+=b; return a; }
+	friend modint operator-(modint a, const modint &b) { a-=b; return a; }
+	friend modint operator*(modint a, const modint &b) { a*=b; return a; }
+	friend modint operator/(modint a, const modint &b) { a/=b; return a; }
+	friend modint operator-(modint a) { if(a.x) a.x = mod - a.x; return a; }
 	friend modint pow(modint a, uint64_t n) { modint p=1; for(; n; n>>=1, a*=a) if(n&1) p*=a; return p; }
-	friend bool operator==(const modint &a, const modint &b){ return a.x==b.x; }
-	friend bool operator!=(const modint &a, const modint &b){ return a.x!=b.x; }
-	friend ostream& operator<<(ostream &o, const modint &m){ return o<<m.x; }
-	explicit operator int(){ return x; }
+	bool operator==(const modint &b) { return x == b.x; }
+	bool operator!=(const modint &b) { return x != b.x; }
+	friend ostream& operator<<(ostream &o, const modint &m) { return o<<m.x; }
+	explicit operator int() { return x; }
 	private: int x;
 };
 using mint = modint<(int)998244353>;
-mint operator""m(unsigned long long x){ return mint(x); }
+mint operator""m(unsigned long long x) { return mint(x); }
