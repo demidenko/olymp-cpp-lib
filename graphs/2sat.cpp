09d9599e92422bcc35b47eb425a99259961e9190
demich <devit15@yandex.ru>
2021-12-07 01:18:19 +0700

    code upd

--- graphs/2sat.cpp
+++ graphs/2sat.cpp
@@ -1,5 +1,6 @@
-struct sat2 {
-	//sat2 a(n); a[i] || !a[j];
+struct sat2 { //sat2 a(n); a[i] || !a[j];
+	sat2(size_t n): n(n), g(n*2) {}
+	
 	struct item {
 		sat2 &s; size_t i; bool value;
 		void operator||(const item &other) {
@@ -9,21 +10,22 @@ struct sat2 {
 	};
 	item operator[](size_t i) { return {*this, i, true}; }
 	
-	sat2(size_t sz): n(sz), g(n*2) { }
 	void add_or(size_t i, bool fi, size_t j, bool fj) {
 		g.add_dir_edge(i+n*fi, j+n*!fj);
 		g.add_dir_edge(j+n*fj, i+n*!fi);
 	}
+	
 	optional<vector<bool>> solve() {
 		vector<bool> res(n);
 		auto cp = strong_connected_components(g).second;
-		for(size_t i=0; i<n; ++i){
+		for(size_t i=0; i<n; ++i) {
 			size_t x = cp[i], y = cp[i+n];
 			if(x == y) return nullopt;
 			res[i] = x > y;
 		}
 		return res;
 	}
+	
 	private:
 	const size_t n;
 	graph g;
--- graphs/graph.cpp
+++ graphs/graph.cpp
@@ -1,20 +1,20 @@
-template<class ...T> struct edge_t : public tuple<size_t,T...> {
-	edge_t(auto&&... args): tuple<size_t,T...>(args...) { }
+template<class... T> struct edge_t : public tuple<size_t,T...> {
+	edge_t(auto&&... args): tuple<size_t,T...>(args...) {}
 	operator size_t() const { return get<0>(*this); }
 };
-template<class...T> struct std::tuple_size<edge_t<T...>> : tuple_size<tuple<size_t,T...>> {};
-template<size_t I, class...T> struct std::tuple_element<I,edge_t<T...>> : tuple_element<I,tuple<size_t,T...>> {};
+template<class... T> struct std::tuple_size<edge_t<T...>> : tuple_size<tuple<size_t,T...>> {};
+template<size_t I, class... T> struct std::tuple_element<I,edge_t<T...>> : tuple_element<I,tuple<size_t,T...>> {};
 
-template<class ...T> struct graph_t {
+template<class... T> struct graph_t {
 	using E = conditional_t<sizeof...(T), edge_t<T...>, size_t>;
 	explicit graph_t(size_t n): g(n) {}
-	void add_dir_edge(size_t from, size_t to, auto&&... args) {
+	void add_dir_edge(size_t from, size_t to, const T&... args) {
 		assert(from<size() && to<size());
 		g[from].emplace_back(to, args...);
 	}
-	void add_edge(size_t x, size_t y, auto&&... args) {
-		add_dir_edge(x, y, args...);
-		add_dir_edge(y, x, args...);
+	void add_edge(size_t i, size_t j, const T&... args) {
+		add_dir_edge(i, j, args...);
+		add_dir_edge(j, i, args...);
 	}
 	const vector<E>& operator[](size_t i) const { return g[i]; }
 	size_t size() const { return g.size(); }
