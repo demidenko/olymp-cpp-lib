7a185f695082e8547ddc10c895cfc9a23ebaee60
demich <devit15@yandex.ru>
2021-12-06 14:25:43 +0700

    bfs optimized; +short code

--- trees/centriod_decomposition.cpp
+++ trees/centriod_decomposition.cpp
@@ -3,42 +3,37 @@ void centriod_decomposition(
 	const graph_t<T...> &g,
 	auto action //use as [&](const auto &t, size_t centroid, size_t sizeof_subtree)
 ) {
-	size_t n = size(g);
+	const size_t n = size(g); if(n == 0) return ;
 	vector<bool> used(n);
-	vector<size_t> par(n), sub(n);
+	vector<size_t> par(n), sub(n), q(n);
 	
 	auto bfs = [&](size_t start, size_t p) {
 		if(start == -1 || used[start]) return ;
 		par[start] = p;
-		vector q = {start};
-		for(size_t k=0; k<size(q); ++k) {
+		size_t qn = 0;
+		q[qn++] = start;
+		for(size_t k=0; k<qn; ++k) {
 			size_t v = q[k];
 			sub[v] = 1;
 			for(size_t i : g[v]) if(!used[i] && i!=par[v]) {
-				q.push_back(i);
+				q[qn++] = i;
 				par[i] = v;
 			}
 		}
-		for(size_t k=size(q)-1; k; --k){
+		for(size_t k = qn-1; k; --k) {
 			size_t v = q[k];
-			sub[par[v]]+=sub[v];
+			sub[par[v]] += sub[v];
 		}
 	};
 	
 	graph_t<T...> tree(n);
 	auto add_edge = [&tree](auto&&... args) { tree.add_edge(args...); };
-	function<void(size_t)> go = [&](size_t v) {
-		const size_t sz = sub[v];
-		for(size_t nx=v; nx!=-1;) {
-			v = nx;
-			nx = -1;
-			for(size_t i : g[v]) 
-				if(!used[i] && i!=par[v] && sub[i]*2 > sz) {
-					nx = i;
-					break;
-				}
+	function<void(size_t)> go = [&](size_t c) {
+		const size_t sz = sub[c];
+		for(size_t nx=c; nx!=-1;) {
+			c = exchange(nx, -1);
+			for(size_t i : g[c]) if(!used[i] && i!=par[c] && sub[i]*2 > sz) nx = i;
 		}
-		const size_t c = v;
 		used[c] = true;
 		bfs(par[c], c);
 		for(auto &e : g[c]) if(size_t i=e; !used[i]) {
