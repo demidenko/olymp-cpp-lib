c4f454899bf9bab054b007191f292c8311f78894
demich <devit15@yandex.ru>
2021-12-07 14:25:21 +0700

    parallel bs checking improved

--- misc/parallel_binary_search.cpp
+++ misc/parallel_binary_search.cpp
@@ -1,6 +1,7 @@
 auto parallel_binary_search(size_t q, size_t n, auto action) {
 	using ask_type = function<bool(size_t)>;
 	struct holder {
+		static void check(bool cond, const char *msg) { if(!cond) __throw_logic_error(msg); }
 		vector<size_t> result;
 		vector<tuple<size_t,size_t,size_t>> ranges;
 		holder(size_t q, size_t n): result(q), ranges(q), q(q), n(n) {
@@ -13,8 +14,7 @@ auto parallel_binary_search(size_t q, size_t n, auto action) {
 			calc();
 		}
 		void finish() {
-			assert(iter == end(ranges));
-			assert(current_version == n);
+			check(current_version == n, "not all versions are commited");
 			size_t sz = 0;
 			for(const auto &[l, r, qi] : ranges) {
 				if(l < r) ranges[sz++] = {l, r, qi};
@@ -31,13 +31,12 @@ auto parallel_binary_search(size_t q, size_t n, auto action) {
 		ask_type ask;
 		typename decltype(ranges)::iterator iter;
 		void calc() {
-			assert(current_version <= n);
+			check(current_version <= n, "too many versions");
 			if(iter == end(ranges)) return ;
 			size_t m = (get<0>(*iter) + get<1>(*iter)) >> 1;
-			assert(m >= current_version);
 			if(m > current_version) return ;
 			auto rter = iter; 
-			while(rter!=end(ranges) && get<0>(*iter) == get<0>(*rter)) ++rter;
+			while(rter != end(ranges) && get<0>(*iter) == get<0>(*rter)) ++rter;
 			for(auto jter = rter; iter != jter; ) {
 				size_t qi = get<2>(*iter), start_qi = qi;
 				do {
@@ -54,21 +53,29 @@ auto parallel_binary_search(size_t q, size_t n, auto action) {
 		}
 	};
 	
-	struct helper {
-		helper(holder &h): h(h), ask(h) {}
-		void commit() { h.commit(); }
+	struct version_controller {
+		version_controller(holder &h): h(h), ask(h) {}
+		void commit() {
+			holder::check(ask.is_set, "ask is not set");
+			h.commit();
+		}
 		private: holder &h;
-		struct subhelper {
-			subhelper(holder &h): h(h) {}
-			void operator=(ask_type f) { h.set_ask(f); }
-			private: holder &h;
+		struct helper {
+			helper(holder &h): h(h), is_set(false) {}
+			void operator=(ask_type f) {
+				holder::check(!is_set, "ask already set in this scope");
+				is_set = true;
+				h.set_ask(f);
+			}
+			private: holder &h; bool is_set;
+			friend version_controller;
 		};
-		public: subhelper ask;
+		public: helper ask;
 	};
 	
 	holder h(q, n);
 	while(!empty(h.ranges)) {
-		action(helper{h});
+		action(version_controller{h});
 		h.finish();
 	}
 	
