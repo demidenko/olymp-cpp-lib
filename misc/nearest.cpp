35095a52a327bd3c61f0134247fc7dbc554fea09
demich <devit15@yandex.ru>
2021-12-10 16:31:37 +0700

    nearest: more optimal version

--- misc/nearest.cpp
+++ misc/nearest.cpp
@@ -1,23 +1,10 @@
-/*
-	vector<int> a;
-	auto next_less = 
-		nearest<less, 1>(a);
-	auto prev_greater_or_equal = 
-		nearest<greater_equal, -1>(a);
-*/
-
-template<template<typename> class C, int dir, typename T>
-vector<int> nearest(const vector<T> &v){
-	static constexpr C<T> cmp;
+template<template<class> class C, int dir, class T>
+vector<int> nearest(const vector<T> &v) {
 	static_assert(abs(dir) == 1);
-	int n = size(v), fin = dir>0 ? n : -1;
-	vector<int> s, res(n, fin);
-	for(int i = dir>0 ? 0 : n-1; i!=fin; i+=dir){
-		while(!empty(s) && cmp(v[i],v[s.back()])){
-			res[s.back()] = i;
-			s.pop_back();
-		}
-		s.push_back(i);
-	}
-	return res;
+	static constexpr C<T> cmp{};
+	int n = size(v), out = dir < 0 ? -1 : n;
+	vector<int> f(n);
+	for(int i = out-dir, k = 0; k < n; ++k, i-=dir)
+		for(f[i] = i+dir; f[i]!=out && !cmp(v[f[i]], v[i]); f[i] = f[f[i]]) ;
+	return f;
 }
