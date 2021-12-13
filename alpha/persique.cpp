3cdccce8090506a8acf5fa82658d1ab5df261857
demich <devit15@yandex.ru>
2021-12-14 01:19:26 +0700

    persique rewrited

--- alpha/persique.cpp
+++ alpha/persique.cpp
@@ -1,73 +1,81 @@
-/*
-	persistent deque by babin74
-*/
-
-template<typename T>
-struct persique {
+template<class T> struct persique {
+	/*
+		persistent deque 
+		invented by babin74
+	*/
+	private:
+	struct node_base {
+		virtual size_t size();
+		virtual const T& get_value(size_t i);
+		virtual node_base* push_front(const T &x);
+		virtual node_base* push_back(const T &x);
+		virtual node_base* pop_front();
+		virtual node_base* pop_back();
+	};
+	struct node_empty;
+	struct node_one;
 	
-	size_t sz;
-	size_t size() { return sz; }
+	struct node: node_base {
+		node_base *p0, *p1;
+		const size_t sz;
+		size_t size() { return sz; }
+		node(node_base *q0, node_base *q1): p0(q0), p1(q1), sz(p0->size() + p1->size()) {}
+		const T& get_value(size_t i) {
+			assert(i < sz);
+			return i%2 ? p1->get_value(i>>1) : p0->get_value(i>>1);
+		}
+		node *push_front(const T &x) {
+			return new node(p1->push_front(x), p0);
+		}
+		node *push_back(const T &x) {
+			return sz % 2 ? new node(p0, p1->push_back(x)) : new node(p0->push_back(x), p1);
+		}
+		node_base* pop_front() {
+			if(sz == 2) return p1;
+			return new node(p1, p0->pop_front());
+		};
+		node_base* pop_back() {
+			if(sz == 2) return p0;
+			return sz % 2 ? new node(p0->pop_back(), p1) : new node(p0, p1->pop_back());
+		}
+	};
 	
-	persique *p0, *p1;
-	T value;
+	struct node_one: node_base {
+		const T value;
+		node_one(const T &x): value(x) {}
+		size_t size() { return 1; }
+		const T& get_value(size_t) { return value; }
+		node* push_front(const T &x) { return new node(new node_one(x), this); }
+		node* push_back(const T &x) { return new node(this, new node_one(x)); }
+		node_empty* pop_front() { return empty_node; }
+		node_empty* pop_back() { return empty_node; }
+	};
 	
-	persique() {
-		sz = 0;
-	}
+	struct node_empty: node_base {
+		size_t size() { return 0; }
+		const T& get_value(size_t) { assert(false); }
+		node_one* push_front(const T &x) { return new node_one(x); }
+		node_one* push_back(const T &x) { return new node_one(x); }
+		node_base* pop_front() { assert(false); }
+		node_base* pop_back() { assert(false); }
+	};
+	static inline node_empty *empty_node = new node_empty();
 	
-	persique(const T &x) {
-		p1 = p0 = nullptr;
-		value = x;
-		sz = 1;
-	}
+	node_base *p;
+	persique(node_base *t): p(t) {}
 	
-	persique(persique *q0, persique *q1) {
-		p0 = q0;
-		p1 = q1;
-		sz = p0->sz + p1->sz;
-	}
+	public:
+	persique(): p(empty_node) {}
 	
-	const T operator[](size_t i) {
-		assert(sz!=0);
-		assert(i<sz);
-		if(sz == 1) return value;
-		if(i%2==0) return p0->operator[](i/2);
-		return p1->operator[](i/2);
-	}
+	size_t size() const { return p->size(); }
+	bool empty() const { return p->size() == 0; }
 	
-	persique *set_value(size_t i, const T &x){
-		assert(sz!=0);
-		assert(i<sz);
-		if(sz == 1) return new persique(x);
-		if(i%2==0) return new persique(p0->set_value(i/2, x), p1);
-		return new persique(p0, p1->set_value(i/2, x));
-	}
+	persique push_front(const T &x) const { return p->push_front(x); }
+	persique push_back(const T &x) const { return p->push_back(x); }
+	persique pop_front() const { return p->pop_front(); }
+	persique pop_back() const { return p->pop_back(); }
 	
-	persique* push_back(const T &x) {
-		if(sz == 0) return new persique(x);
-		if(sz == 1) return new persique(this, new persique(x));
-		if(sz%2==0) return new persique(p0->push_back(x), p1);
-		return new persique(p0, p1->push_back(x));
-	}
-	
-	persique* push_front(const T &x){
-		if(sz == 0) return new persique(x);
-		if(sz == 1) return new persique(new persique(x), this);
-		return new persique(p1->push_front(x), p0);
-	}
-	
-	persique* pop_back(){
-		assert(sz!=0);
-		if(sz == 1) return new persique();
-		if(sz == 2) return p0;
-		if(sz%2 == 0) return new persique(p0, p1->pop_back());
-		return new persique(p0->pop_back(), p1);
-	}
-	
-	persique* pop_front(){
-		assert(sz!=0);
-		if(sz == 1) return new persique();
-		if(sz == 2) return p1;
-		return new persique(p1, p0->pop_front());
-	}
+	const T& operator[](size_t i) const { return p->get_value(i); }
+	const T& front() const { return operator[](0); }
+	const T& back() const { return operator[](size()-1); }
 };
