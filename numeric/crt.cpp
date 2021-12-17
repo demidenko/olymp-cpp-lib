c93910d5fbc13957f8a967c9af8220aeda208c6c
demich <devit15@yandex.ru>
2021-12-18 02:23:33 +0700

    code upd

--- numeric/crt.cpp
+++ numeric/crt.cpp
@@ -1,23 +1,21 @@
 struct crt_device {
-	template<class T, int mod, int ...mods> 
-	static T evaluate(const modint<mod> &a0, const modint<mods>& ...a) {
-		return evaluate<T>(((a - (int)a0) * get_inv<mod,mods>())...) * T((int)mod) + T((int)a0);
+	template<class T, int mod0, int ...mod> 
+	static T evaluate(const modint<mod0> &a0, const modint<mod>& ...a) {
+		return evaluate<T>(((a - (int)a0) * get_inv<mod0, mod>())...) * T((int)mod0) + T((int)a0);
 	}
 	
-	template<class T, int mod> 
-	static T evaluate(const modint<mod> &a0) {
+	template<class T, int mod> static T evaluate(const modint<mod> &a0) {
 		return T((int)a0);
 	}
 	
-	template<class T, int ...mod>
-	static vector<T> evaluate(const vector<modint<mod>>& ...a) {
-		vector<T> res((size(a),...));
-		for(size_t i=0; i<size(res); ++i) res[i] = evaluate<T>(a[i]...);
+	template<class T, class mint0, class ...mint>
+	static vector<T> evaluate(const vector<mint0> &a0, const vector<mint>& ...a) {
+		vector<T> res(size(a0)); assert(((size(a) == size(a0)) && ...));
+		for(size_t i=0; i<size(res); ++i) res[i] = evaluate<T>(a0[i], a[i]...);
 		return res;
 	}
 	
-	private:
-	template<int x, int mod> static modint<mod> get_inv() {
+	private: template<int x, int mod> static inline auto get_inv() {
 		static const auto inv = 1 / modint<mod>{x};
 		return inv;
 	}
