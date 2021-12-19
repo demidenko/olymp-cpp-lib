873fab2f80cd5d4adb68f9b53971745e1ffa2f94
demich <devit15@yandex.ru>
2021-12-20 03:04:32 +0700

    define for +-*/ overloads

--- numeric/modint.cpp
+++ numeric/modint.cpp
@@ -2,14 +2,11 @@ template<decltype(auto) mod> struct modint { static_assert(is_same_v<remove_refe
 	modint(): x(0) {}
 	template<class T, class=enable_if_t<is_integral_v<T>>> modint(T val): x(val%mod) { if(x<0) x+=mod; }
 	static constexpr int get_mod() { return mod; }
-	void operator+=(const modint &b) { x+=b.x; if(x>=mod) x-=mod; }
-	void operator-=(const modint &b) { x-=b.x; if(x<0) x+=mod; }
-	void operator*=(const modint &b) { x = int64_t(x)*b.x %mod; }
-	void operator/=(const modint &b) { auto i=b.inverse(); assert(i); x = int64_t(i->x)*x %mod; }
-	friend modint operator+(modint a, const modint &b) { a+=b; return a; }
-	friend modint operator-(modint a, const modint &b) { a-=b; return a; }
-	friend modint operator*(modint a, const modint &b) { a*=b; return a; }
-	friend modint operator/(modint a, const modint &b) { a/=b; return a; }
+	#define __op(O, E, F) modint& operator E(const modint &b) { F return *this; } friend modint operator O(modint a, const modint &b) { return a E b; }
+	__op(+, +=,  x+=b.x; if(x>=mod) x-=mod; )
+	__op(-, -=,  x-=b.x; if(x<0) x+=mod; )
+	__op(*, *=,  x = int64_t(x)*b.x %mod; )
+	__op(/, /=,  auto i=b.inverse(); assert(i); x = int64_t(i->x)*x %mod; )
 	friend modint operator-(modint a) { if(a.x) a.x = mod - a.x; return a; }
 	friend modint pow(modint a, uint64_t n) { modint p=1; for(; n; n>>=1, a*=a) if(n&1) p*=a; return p; }
 	optional<modint> inverse() const {
