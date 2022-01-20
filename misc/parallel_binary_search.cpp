302c4a85f507848c0c75f7d7c1bc20453081721e
demidenko <devit15@yandex.ru>
2022-01-21 01:39:34 +0700

    code update

--- misc/parallel_binary_search.cpp
+++ misc/parallel_binary_search.cpp
@@ -1,79 +1,64 @@
-auto parallel_binary_search(size_t q, size_t n, auto action) {
+auto parallel_binary_search(size_t queries_count, size_t versions_count, auto action) {
 	using ask_type = function<bool(size_t)>;
 	struct holder {
-		static void check(bool cond, const char *msg) { if(!cond) __throw_logic_error(msg); }
+		static void ensure(bool cond, const char *msg) { if(!cond) __throw_logic_error(msg); }
+		struct range { size_t l, r, qi; };
+		vector<range> ranges;
 		vector<size_t> result;
-		vector<tuple<size_t,size_t,size_t>> ranges;
-		holder(size_t q, size_t n): result(q), ranges(q), q(q), n(n) {
+		holder(size_t q, size_t n): ranges(q), result(q), n(n) {
 			for(size_t i=0; i<q; ++i) ranges[i] = {0, n+1, i};
 		}
 		void set_ask(ask_type f) {
+			ensure(!ask.has_value(), "ask already set in this scope");
 			ask = f;
 			iter = begin(ranges);
 			current_version = 0;
 			calc();
 		}
 		void finish() {
-			check(current_version == n, "not all versions are commited");
-			size_t sz = 0;
-			for(const auto &[l, r, qi] : ranges) {
-				if(l < r) ranges[sz++] = {l, r, qi};
-				else result[qi] = r;
-			}
-			ranges.resize(sz);
+			ensure(current_version == n, "not all versions are commited");
+			ask.reset();
+			auto it = begin(ranges);
+			for(auto &t : ranges) if(auto &[l, r, qi] = t; l < r) *it++ = t; else result[qi] = r;
+			ranges.erase(it, end(ranges));
 		}
 		void commit() {
+			ensure(ask.has_value(), "ask is not set");
 			current_version++;
 			calc();
 		}
-		private: 
-		size_t q, n, current_version;
-		ask_type ask;
-		typename decltype(ranges)::iterator iter;
+		private: size_t n, current_version;
+		optional<ask_type> ask;
+		typename decltype(ranges)::iterator iter, rter;
 		void calc() {
-			check(current_version <= n, "too many versions");
+			ensure(current_version <= n, "too many versions");
 			if(iter == end(ranges)) return ;
-			size_t m = (get<0>(*iter) + get<1>(*iter)) >> 1;
+			const size_t m = (iter->l + iter->r) >> 1;
 			if(m > current_version) return ;
-			auto rter = iter; 
-			while(rter != end(ranges) && get<0>(*iter) == get<0>(*rter)) ++rter;
-			for(auto jter = rter; iter != jter; ) {
-				size_t qi = get<2>(*iter), start_qi = qi;
-				do {
-					if(ask(qi)) {
-						swap(get<2>(*iter), qi);
-						get<1>(*iter++) = m;
-					} else {
-						swap(get<2>(*--jter), qi);
-						get<0>(*jter) = m+1;
-					}
-				} while(qi != start_qi);
-			}
+			for(rter = iter; rter != end(ranges) && rter->l == iter->l; ++rter);
+			for(auto jter = rter; iter != jter; ) 
+				for(size_t qi = iter->qi, start_qi = qi; ; ) {
+					if((*ask)(qi)) swap(iter->qi, qi), (iter++)->r = m;
+					else swap((--jter)->qi, qi), jter->l = m+1;
+					if(qi == start_qi) break ;
+				}
 			iter = rter;
 		}
 	};
 	
 	struct version_controller {
-		version_controller(holder &h): h(h), ask(h) {}
-		void commit() {
-			holder::check(ask.is_set, "ask is not set");
-			h.commit();
-		}
+		explicit version_controller(holder &h): h(h), ask(h) {}
+		void commit() { h.commit(); }
 		private: holder &h;
 		struct helper {
-			helper(holder &h): h(h), is_set(false) {}
-			void operator=(ask_type f) {
-				holder::check(!is_set, "ask already set in this scope");
-				is_set = true;
-				h.set_ask(f);
-			}
-			private: holder &h; bool is_set;
-			friend version_controller;
+			helper(holder &h): h(h) {}
+			void operator=(ask_type f) { h.set_ask(f); }
+			private: holder &h;
 		};
 		public: helper ask;
 	};
 	
-	holder h(q, n);
+	holder h(queries_count, versions_count);
 	while(!empty(h.ranges)) {
 		action(version_controller{h});
 		h.finish();
