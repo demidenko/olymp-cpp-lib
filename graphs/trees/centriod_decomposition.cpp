33bbf5238be8d8c456820ca1de3193bd8e12160f
demidenko <devit15@yandex.ru>
2022-01-26 17:05:09 +0700

    centroid decomp huge improve

--- graphs/trees/centriod_decomposition.cpp
+++ graphs/trees/centriod_decomposition.cpp
@@ -1,45 +1,30 @@
 template<class... T>
-void centriod_decomposition(
-	const graph_t<T...> &g,
-	auto action //use as [&](auto &g, size_t centroid, size_t sizeof_subtree)
-) {
+void centriod_decomposition(const graph_t<T...> &g, auto action) {
 	const size_t n = size(g); if(n == 0) return ;
-	vector<bool> used(n);
-	vector<size_t> par(n), sub(n), q(n);
-	
-	auto bfs = [&](size_t start, size_t p) {
-		if(start == n || used[start]) return ;
-		par[start] = p;
-		size_t qn = 0;
-		q[qn++] = start;
-		for(size_t k=0; k<qn; ++k) {
-			size_t v = q[k];
-			sub[v] = 1;
-			for(size_t i : g[v]) if(!used[i] && i!=par[v]) {
-				q[qn++] = i;
-				par[i] = v;
-			}
-		}
-		while(--qn) sub[par[q[qn]]] += sub[q[qn]];
-	};
-	
+	vector<size_t> sub(n, 1), p(n, -1), q(n);
+	for(size_t k=0, qn=1; k<qn; ++k) {
+		size_t v = q[k];
+		for(size_t i : g[v]) if(i!=p[v]) q[qn++] = i, p[i] = v;
+	}
+	for(size_t i=n-1; i; --i) sub[p[q[i]]] += sub[q[i]];
 	graph_t<T...> tree(n);
 	auto add_edge = [&tree](auto&&... args) { tree.add_edge(args...); };
+	vector<bool> used(n);
 	function<void(size_t)> go = [&](size_t c) {
 		const size_t sz = sub[c];
-		for(size_t nx=c; nx != n; ) {
-			c = exchange(nx, n);
-			for(size_t i : g[c]) if(!used[i] && i!=par[c] && sub[i]*2 > sz) nx = i;
-		}
+		for(size_t pr = -1; exchange(pr,c) != c; )
+			for(size_t i : g[c]) if(!used[i] && sub[i]*2 > sz) {
+				sub[c] -= exchange(sub[i], sz);
+				c = i;
+				break;
+			}
 		used[c] = true;
-		bfs(par[c], c);
 		for(auto &e : g[c]) if(size_t i=e; !used[i]) {
 			go(i);
 			std::apply(add_edge, tuple_cat(tuple{c}, tuple<size_t,T...>{e}));
 		}
 		action(std::as_const(tree), c, sz);
 	};
-	
-	bfs(0, n);
 	go(0);
 }
+//use action as [&](auto &g, size_t centroid, size_t sizeof_subtree)
