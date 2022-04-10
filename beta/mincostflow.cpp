b9d66e4aaaf1fae1619897f0a57edd8b20ba2921
demidenko <devit15@yandex.ru>
2022-04-11 00:50:30 +0700

    struct instead of pair

--- beta/mincostflow.cpp
+++ beta/mincostflow.cpp
@@ -1,7 +1,7 @@
 template<class F, class C>
-struct mcmf_graph {
-	mcmf_graph(size_t n): mcmf_graph(n, 0, n-1) {}
-	mcmf_graph(size_t n, size_t s, size_t t): g(n), S(s), T(t), p(n) {}
+struct mincost_flow_graph {
+	mincost_flow_graph(size_t n): mincost_flow_graph(n, 0, n-1) {}
+	mincost_flow_graph(size_t n, size_t s, size_t t): g(n), S(s), T(t), p(n) {}
 	
 	void add_edge(size_t from, size_t to, F cap, C cost) {
 		assert(0 <= from && from < size(g));
@@ -12,11 +12,11 @@ struct mcmf_graph {
 		edges.push_back({from, 0, 0, -cost});
 	}
 	
-	pair<F, C> push() { return push(numeric_limits<F>::max()); }
-	pair<F, C> push(F flow) {
-		const size_t n = size(p);
-		vector<size_t> fr(n, -1);
-		vector<C> d(n, numeric_limits<C>::max());
+	struct result { F flow; C cost; };
+	result push() { return push(numeric_limits<F>::max()); }
+	result push(F flow) {
+		vector<size_t> fr(size(p), -1);
+		vector<C> d(size(p), numeric_limits<C>::max());
 		priority_queue<pair<C,size_t>, vector<pair<C,size_t>>, greater<pair<C,size_t>>> q;
 		q.emplace(d[S] = 0, S);
 		while(!empty(q)) {
@@ -43,7 +43,7 @@ struct mcmf_graph {
 			edges[k^1].flow -= flow;
 		}
 		
-		for(size_t i=0; i<n; ++i) p[i] += d[i];
+		for(size_t i=0; i<size(p); ++i) p[i] += d[i];
 		return {flow, cost};
 	}
 	
