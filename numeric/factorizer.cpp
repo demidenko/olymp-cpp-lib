49bd42b31a4d0a1603745cfff7995800a077f4a4
demidenko <devit15@yandex.ru>
2022-04-17 13:10:14 +0700

    code

--- numeric/factorizer.cpp
+++ numeric/factorizer.cpp
@@ -1,10 +1,10 @@
 struct factorizer {
 	vector<int> primes, md;
 	factorizer(int n): md(n+1) {
-		for(int i=2;i<=n;++i){
-			if(!md[i]) primes.push_back(md[i]=i);
-			for(int p : primes){
-				if(p > md[i] || p*i > n) break;
+		for(int i=2; i<=n; ++i) {
+			if(!md[i]) md[i] = primes.emplace_back(i);
+			for(int p : primes) {
+				if(p > md[i] || i*p > n) break;
 				md[i*p] = p;
 			}
 		}
@@ -12,7 +12,7 @@ struct factorizer {
 	
 	auto factorize(int x) {
 		static pair<int,int> f[11], *e;
-		for(e = f; x>1; ++e){
+		for(e = f; x>1; ++e) {
 			int p = md[x], q = 0;
 			do ++q, x/=p; while(md[x]==p);
 			*e = {p, q};
@@ -23,11 +23,10 @@ struct factorizer {
 	auto divisors(int x) {
 		static int divs[1500], dn;
 		divs[0] = dn = 1;
-		for(int p=0,k,l; x!=1; x/=p){
-			if(int c=md[x];c!=p) k=dn, l=0, p=c;
-			for(int i=k;i--;) divs[dn++] = divs[l++]*p;
+		for(int p=0,k=0,l=0; x>1; x/=p) {
+			if(int c=md[x]; c!=p) p = c, k = dn, l = 0;
+			for(int i=k;i--;) divs[dn++] = divs[l++] * p;
 		}
 		return vector<int>(divs, divs+dn);
 	}
-	
 };
