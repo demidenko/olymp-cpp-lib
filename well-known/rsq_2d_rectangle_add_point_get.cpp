25a858e214a43e6699b74bf79ca48c3883c7bdca
demidenko <devit15@yandex.ru>
2022-06-26 23:52:17 +0700

    code upd

--- well-known/rsq_2d_rectangle_add_point_get.cpp
+++ well-known/rsq_2d_rectangle_add_point_get.cpp
@@ -1,23 +1,19 @@
-template<class T, class P>
+template<class T, class P, template<class> class rsq>
 struct rsq_2d_rectangle_add_point_get {
 	rsq_2d_rectangle_add_point_get(vector<pair<P,P>> points): ys(size(points)) {
 		for(size_t k=0; k<size(ys); ++k) ys[k] = points[k].second;
 		sort(begin(ys), end(ys));
-		make_unique(ys);
-		t.resize(size(ys));
+		t.resize(unique_sz(ys));
 		sort(begin(points), end(points));
 		for(auto [x, y] : points)
 		for(size_t v=lb(ys,y); v<size(t); v|=v+1) t[v].first.push_back(x);
-		for(auto &[xs, f] : t) {
-			make_unique(xs);
-			f = rsq<T>(size(xs));
-		}
+		for(auto &[xs, f] : t) f = rsq<T>(unique_sz(xs));
 	}
 	
 	T get_value(const P &x, const P &y) const {
 		T res{};
 		for(size_t v=lb(ys, y); v<size(ys); v|=v+1) 
-			res += t[v].second.sum_suf(lb(t[v].first, x));
+			res += t[v].second(lb(t[v].first, x), size(t[v].first));
 		return res;
 	}
 	
@@ -36,6 +32,6 @@ struct rsq_2d_rectangle_add_point_get {
 	
 	vector<P> ys;
 	vector<pair<vector<P>,rsq<T>>> t;
-	static void make_unique(vector<P> &v) { v.resize(unique(begin(v),end(v))-begin(v)); }
-	static inline size_t lb(const vector<P> &v, const P &x) { return lower_bound(begin(v), end(v), x) - begin(v); }
+	static size_t unique_sz(auto&&v) { size_t sz = unique(begin(v), end(v)) - begin(v); v.resize(sz); return sz; }
+	static inline size_t lb(auto&&v, auto&&val) { return lower_bound(begin(v), end(v), val) - begin(v); }
 };
