abcd87054d1c7fa4a9a0483b0e02274234f1bf86
demidenko <devit15@yandex.ru>
2022-06-26 23:40:26 +0700

    code upd

--- well-known/rsq_2d_point_add_rectangle_sum.cpp
+++ well-known/rsq_2d_point_add_rectangle_sum.cpp
@@ -1,26 +1,22 @@
-template<class T, class P>
+template<class T, class P, template<class> class rsq>
 struct rsq_2d_point_add_rectangle_sum {
 	rsq_2d_point_add_rectangle_sum(vector<pair<P,P>> points): ys(size(points)) {
 		for(size_t k=0; k<size(ys); ++k) ys[k] = points[k].second;
 		sort(begin(ys), end(ys));
-		make_unique(ys);
-		t.resize(size(ys));
+		t.resize(unique_sz(ys));
 		sort(begin(points), end(points));
 		for(auto [x, y] : points)
 		for(size_t v=lb(ys,y); v<size(t); v|=v+1) t[v].first.push_back(x);
-		for(auto &[xs, f] : t) {
-			make_unique(xs);
-			f = rsq<T>(size(xs));
-		}
+		for(auto &[xs, f] : t) f = rsq<T>(unique_sz(xs));
 	}
 	
 	T get_sum(const P &xl, const P &xr, const P &yl, const P &yr) {
-		if(xl>=xr || yl>=yr) return T{};
+		if(xl >= xr || yl >= yr) return T{};
 		return get_sum(xl, xr, yr) - get_sum(xl, xr, yl);
 	}
 	
 	void add(const P &x, const P &y, const T &val) {
-		for(size_t v=lb(ys, y); v<size(ys); v|=v+1) 
+		for(size_t v = lb(ys, y); v < size(ys); v|=v+1) 
 			t[v].second.add(lb(t[v].first, x), val);
 	}
 	
@@ -35,6 +31,6 @@ struct rsq_2d_point_add_rectangle_sum {
 	
 	vector<pair<vector<P>,rsq<T>>> t;
 	vector<P> ys;
-	static void make_unique(vector<P> &v) { v.resize(unique(begin(v),end(v))-begin(v)); }
-	static inline size_t lb(const vector<P> &v, const P &y) { return lower_bound(begin(v), end(v), y) - begin(v); }
+	static size_t unique_sz(auto&&v) { size_t sz = unique(begin(v), end(v)) - begin(v); v.resize(sz); return sz; }
+	static inline size_t lb(auto&&v, auto&&val) { return lower_bound(begin(v), end(v), val) - begin(v); }
 };
