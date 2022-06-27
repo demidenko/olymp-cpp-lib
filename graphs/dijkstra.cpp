ed39b1e3b33d61a44228001c0042ebe040112112
demidenko <devit15@yandex.ru>
2022-06-27 18:48:35 +0700

    shorting

--- graphs/dijkstra.cpp
+++ graphs/dijkstra.cpp
@@ -1,10 +1,9 @@
 template<class S, class W>
 auto dijkstra(const S &start, auto &&gen) {
 	/*unordered_*/map<S, pair<W,S>> dist{{start,{0,start}}};
-	set<pair<W,S>> q{{0,start}};
-	while(!empty(q)) {
+	for(set<pair<W,S>> q{{0,start}}; !empty(q); ) {
 		auto [dv, v] = move(q.extract(begin(q)).value());
-		gen(as_const(v), [&dist, &q, &dv, &v](const S &t, W cost) {
+		gen(as_const(v), [&](const S &t, W cost) {
 			const W cur = dv + cost;
 			if(auto it = dist.find(t); it == end(dist)) {
 				dist.emplace(t, pair{cur, v});
