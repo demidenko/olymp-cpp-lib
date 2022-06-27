540c9059cf1312175aeef8cfed06eaef2a5669a1
demidenko <devit15@yandex.ru>
2022-06-27 20:22:29 +0700

    upd for aggregator

--- well-known/area_of_union_of_rectangles.cpp
+++ well-known/area_of_union_of_rectangles.cpp
@@ -1,5 +1,5 @@
 ll area_of_union_of_rectangles(
-	//all inclusive
+	//[ax, ay, bx, by] all inclusive
 	const vector<tuple<int,int,int,int>> &rects
 ) {
 	struct add_op {
@@ -12,18 +12,17 @@ ll area_of_union_of_rectangles(
 	struct icnt {
 		int x, cnt;
 		icnt(): icnt(0,0) {}
-		icnt(int value, int __count = 1): x(value), cnt(__count) { }
-		icnt(const icnt &a, const icnt &b) {
-			x = min(a.x, b.x);
-			cnt = 0;
+		icnt(int val, int cnt = 1): x(val), cnt(cnt) {}
+		icnt(const icnt &a, const icnt &b): x(min(a.x,b.x)), cnt(0) {
 			if(a.x == x) cnt += a.cnt;
 			if(b.x == x) cnt += b.cnt;
 		}
-		icnt(const icnt &a, const add_op &o, size_t): x(a.x+o.a), cnt(a.cnt) {}
+		void apply(const add_op &o, size_t) { x += o.a; }
 	};
 	
 	vector<int> ys;
 	for(auto [ax, ay, bx, by] : rects) {
+		assert(ax <= bx && ay <= by);
 		ys.push_back(ay);
 		ys.push_back(by+1);
 	}
@@ -33,11 +32,11 @@ ll area_of_union_of_rectangles(
 	ys.resize(unique(begin(ys), end(ys)) - begin(ys));
 	const int H = ys.back() - ys[0];
 	
-	segment_tree<icnt,add_op> t(size(ys)-1, [&](size_t i) {
+	aggregator<icnt,add_op> t(size(ys)-1, [&](size_t i) {
 		return icnt{0, ys[i+1]-ys[i]};
 	});
 	
-	vector<tuple<int,int,int,int>> events;
+	vector<tuple<int,char,size_t,size_t>> events;
 	events.reserve(size(rects)*2);
 	for(auto [ax, ay, bx, by] : rects) {
 		size_t sl = lower_bound(begin(ys), end(ys), ay) - begin(ys);
@@ -45,16 +44,16 @@ ll area_of_union_of_rectangles(
 		events.emplace_back(ax, +1, sl, sr);
 		events.emplace_back(bx+1, -1, sl, sr);
 	}
-	sort(begin(events), end(events), [&](auto &e1, auto &e2) {
+	sort(begin(events), end(events), [](auto &e1, auto &e2) {
 		return get<0>(e1) < get<0>(e2);
 	});
 	
 	ll area = 0, lx = 0;
 	for(auto [x, tp, sl, sr] : events) {
-		auto cur = t();
+		auto &cur = t();
 		ll filled = cur.x == 0 ? H - cur.cnt : H;
 		area += filled * (x - lx);
-		t.apply(sl, sr, tp);
+		t.apply(sl, sr, add_op{tp});
 		lx = x;
 	}
 	
