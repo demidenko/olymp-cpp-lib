71aad0def27c140f0160d1cb7efdb4e140f7b7e2
demidenko <devit15@yandex.ru>
2022-07-20 02:08:11 +0700

    code short

--- misc/parallel_binary_search.cpp
+++ misc/parallel_binary_search.cpp
@@ -13,41 +13,36 @@ auto parallel_binary_search(size_t queries_count, size_t versions_count, auto &&
 	vector<range> ranges(queries_count);
 	for(size_t i=0; i<queries_count; ++i) ranges[i] = {0, versions_count + 1, i};
 	
-	optional<pred_t> pred;
-	size_t current_version;
-	decltype(begin(ranges)) iter, rter;
-	
-	auto calc = [&] {
-		ensure(pred.has_value(), "pred is not set");
-		ensure(current_version <= versions_count, "too many versions");
-		if(iter == end(ranges)) return ;
-		const size_t m = (iter->l + iter->r) >> 1;
-		if(m > current_version) return ;
-		for(rter = iter; rter != end(ranges) && rter->l == iter->l; ++rter);
-		for(auto jter = rter; iter != jter; ) 
-			for(size_t qi = iter->qi, start_qi = qi; ; ) {
-				if(!(*pred)(qi)) swap(iter->qi, qi), (iter++)->r = m;
-				else swap((--jter)->qi, qi), jter->l = m+1;
-				if(qi == start_qi) break ;
-			}
-		iter = rter;
-	};
-	
 	vector<size_t> result(queries_count);
 	while(!empty(ranges)) {
+		optional<pred_t> pred;
+		size_t current_version = 0;
+		auto it = begin(ranges);
+		auto calc = [&] {
+			ensure(pred.has_value(), "pred is not set");
+			ensure(current_version <= versions_count, "too many versions");
+			if(it == end(ranges)) return ;
+			const size_t m = (it->l + it->r) >> 1;
+			if(m > current_version) return ;
+			auto lt = it, rt = it;
+			while(rt != end(ranges) && rt->l == lt->l) ++rt;
+			for(it = rt; lt != rt; ) 
+				for(size_t qi = lt->qi, start_qi = qi; ; ) {
+					if((*pred)(qi)) swap((--rt)->qi, qi), rt->l = m + 1;
+					else swap(lt->qi, qi), (lt++)->r = m;
+					if(qi == start_qi) break ;
+				}
+		};
 		action(vc {
 			assign_helper([&](pred_t f) {
 				ensure(!pred.has_value(), "pred already set in this scope");
 				pred = f;
-				iter = begin(ranges);
-				current_version = 0;
 				calc();
 			}),
 			[&] { ++current_version; calc(); }
 		});
 		ensure(current_version == versions_count, "not all versions are commited");
-		pred.reset();
-		auto it = begin(ranges);
+		it = begin(ranges);
 		for(auto &t : ranges) if(t.l < t.r) *it++ = t; else result[t.qi] = t.r;
 		ranges.erase(it, end(ranges));
 	}
