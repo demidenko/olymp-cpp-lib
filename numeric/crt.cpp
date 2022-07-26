3a094781b0beee6b8d0ff02b39149009782c8f78
demidenko <devit15@yandex.ru>
2022-07-26 17:27:43 +0700

    code ref

--- numeric/crt.cpp
+++ numeric/crt.cpp
@@ -1,14 +1,12 @@
 struct crt_device {
 	template<class T, int mod0, int ...mod> 
 	static T evaluate(const modint<mod0> &a0, const modint<mod>& ...a) {
+		if constexpr (sizeof...(a) == 0) return T(*a0); else
 		return evaluate<T>(((a - *a0) * inv<mod0, mod>)...) * T(mod0) + T(*a0);
 	}
 	
-	template<class T, int mod>
-	static T evaluate(const modint<mod> &a0) { return T(*a0); }
-	
-	template<class T, class mint0, class ...mint>
-	static vector<T> evaluate(const vector<mint0> &a0, const vector<mint>& ...a) {
+	template<class T, int mod0, int ...mod>
+	static vector<T> evaluate(const vector<modint<mod0>> &a0, const vector<modint<mod>>& ...a) {
 		vector<T> res(size(a0)); assert(((size(a) == size(a0)) && ...));
 		for(size_t i=0; i<size(res); ++i) res[i] = evaluate<T>(a0[i], a[i]...);
 		return res;
