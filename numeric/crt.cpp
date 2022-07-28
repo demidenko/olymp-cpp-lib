5689ee44765784370a22c758291eda755e63cb80
demidenko <devit15@yandex.ru>
2022-07-28 23:24:39 +0700

    dynamic crt

--- numeric/crt.cpp
+++ numeric/crt.cpp
@@ -1,4 +1,27 @@
 struct crt_device {
+	crt_device(){};
+	crt_device(const vector<int> &mods): mods(mods), invs(size(mods)) {
+		for(size_t i=0; i<size(mods); ++i) {
+			tmod = mods[i];
+			invs[i].resize(i);
+			for(size_t j=0; j<i; ++j) invs[i][j] = *modint<(tmod)>{mods[j]}.inverse().value();
+		}
+	}
+	
+	template<class T>
+	T evaluate(vector<int> a) const {
+		assert(size(a) == size(mods));
+		for(size_t i=0; i<size(a); ++i) {
+			tmod = mods[i];
+			modint<(tmod)> cur{a[i]};
+			for(size_t j=0; j<i; ++j) cur -= a[j], cur *= invs[i][j];
+			a[i] = *cur;
+		}
+		T res = 0;
+		for(size_t i=size(a); i--; ) res *= mods[i], res += a[i];
+		return res;
+	}
+	
 	template<class T, int mod0, int ...mod> 
 	static T evaluate(const modint<mod0> &a0, const modint<mod>& ...a) {
 		if constexpr (sizeof...(a) == 0) return T(*a0); else
@@ -12,5 +35,10 @@ struct crt_device {
 		return res;
 	}
 	
-	private: template<int x, int mod> static inline const auto inv =  1 / modint<mod>{x};
+	private: 
+	vector<int> mods;
+	vector<vector<int>> invs;
+	
+	template<int x, int mod> static inline const auto inv =  1 / modint<mod>{x};
+	static inline int tmod;
 };
