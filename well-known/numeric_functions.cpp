a725ac7a479bb7d2b49809ef9ab33c6e79fed2a2
demidenko <devit15@yandex.ru>
2022-07-30 15:42:15 +0700

    generalize & optimize

--- well-known/numeric_functions.cpp
+++ well-known/numeric_functions.cpp
@@ -1,14 +1,14 @@
-//g(n) = a(n) - sum i=2..n g(n/i)
+//g(n) = a(n) - sum i=2..n g(n/i) * b(i)
 //almost O(n ** 2/3)
 template<class T>
-T g(int64_t maxn, auto &&a) {
+T g(int64_t maxn, auto &&a, auto &&b, auto &&sumb) {
 	const int32_t sq = max(sqrt(maxn), pow(maxn / log(maxn + 1), 2./3) * 3);
 	vector<T> small(sq + 1, 0), big(maxn / (sq + 1) + 1);
 	
 	for(int32_t n = 1; n <= sq; ++n) {
 		small[n] += small[n-1] - a(n-1) + a(n);
 		const T dif = small[n] - small[n-1];
-		for(int32_t i = 2; i <= sq / n; ++i) small[i * n] -= dif;
+		for(int32_t i = 2; i <= sq / n; ++i) small[i * n] -= dif * b(i);
 	}
 	
 	for(int32_t d = maxn / (sq + 1) ; d >= 1; --d) {
@@ -16,15 +16,13 @@ T g(int64_t maxn, auto &&a) {
 		const int32_t sn = sqrt(n);
 		T res = a(n);
 		
-		for(int32_t k = 1; k <= sn; ++k) {
-			int64_t r = n / k, l = n / (k + 1) + 1;
-			assert(l > 1);
-			res -= small[k] * T(r - l + 1);
-		}
-		
-		for(int32_t i = 2; i <= n / (sn + 1); ++i) {
-			int64_t k = n / i;
-			res -= (k <= sq ? small[k] : big[maxn / k]);
+		int64_t l, r = n;
+		for(int32_t k = 1; k <= sn; ++k, r = l) {
+			l = n / (k + 1);
+			res -= small[k] * (sumb(r) - sumb(l));
+			if(int32_t i = k + 1; i <= n / (sn + 1)) {
+				res -= (l <= sq ? small[l] : big[maxn / l]) * b(i);
+			}
 		}
 		
 		big[d] = res;
