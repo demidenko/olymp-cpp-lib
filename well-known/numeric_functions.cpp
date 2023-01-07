7626ac3636c01201b418fa0c7e4314d842863a3a
demidenko <devit15@yandex.ru>
2023-01-07 12:33:15 +0700

    int -> uint; less divisions;

--- well-known/numeric_functions.cpp
+++ well-known/numeric_functions.cpp
@@ -1,28 +1,26 @@
 //g(n) = a(n) - sum i=2..n g(n/i) * b(i)
 //almost O(n ** 2/3)
 template<class T>
-T g(int64_t maxn, auto &&a, auto &&b, auto &&sumb) {
-	const int32_t sq = max(sqrt(maxn), pow(maxn / log(maxn + 1), 2./3) * 3);
+T g(uint64_t maxn, auto &&a, auto &&b, auto &&sumb) {
+	const uint32_t sq = max(sqrt(maxn), pow(maxn / log(maxn + 1), 2./3) * 2);
 	vector<T> small(sq + 1, 0), big(maxn / (sq + 1) + 1);
 	
-	for(int32_t n = 1; n <= sq; ++n) {
+	for(uint32_t n = 1; n <= sq; ++n) {
 		small[n] += small[n-1] - a(n-1) + a(n);
 		const T dif = small[n] - small[n-1];
-		for(int32_t i = 2; i <= sq / n; ++i) small[i * n] -= dif * b(i);
+		for(uint32_t i = 2; i * n <= sq; ++i) small[i * n] -= dif * b(i);
 	}
 	
-	for(int32_t d = maxn / (sq + 1) ; d >= 1; --d) {
-		const int64_t n = maxn / d;
-		const int32_t sn = sqrt(n);
+	for(uint32_t d = maxn / (sq + 1) ; d >= 1; --d) {
+		const uint64_t n = maxn / d;
+		const uint32_t sn = sqrt(n), sp = n / (sn + 1);
 		T res = a(n);
 		
-		int64_t l, r = n;
-		for(int32_t k = 1; k <= sn; ++k, r = l) {
+		uint64_t l, r = n;
+		for(uint32_t k = 1; k <= sn; ++k, r = l) {
 			l = n / (k + 1);
 			res -= small[k] * (sumb(r) - sumb(l));
-			if(int32_t i = k + 1; i <= n / (sn + 1)) {
-				res -= (l <= sq ? small[l] : big[maxn / l]) * b(i);
-			}
+			if(k+1 <= sp) res -= (l <= sq ? small[l] : big[maxn / l]) * b(k+1);
 		}
 		
 		big[d] = res;
