2ee5183f56b4d0f8da174debe1107ec9c975fa2e
demidenko <devit15@yandex.ru>
2023-01-22 22:20:21 +0700

    code upd

--- numeric/factorizer.cpp
+++ numeric/factorizer.cpp
@@ -1,7 +1,7 @@
 struct factorizer {
 	vector<int> minp, primes;
-	explicit factorizer(size_t n): minp(n+1) {
-		for(size_t i=2; i<=n; ++i) {
+	explicit factorizer(int n): minp(n+1) {
+		for(int i=2; i<=n; ++i) {
 			if(!minp[i]) minp[i] = primes.emplace_back(i);
 			for(int p : primes) 
 				if(p <= minp[i] && i*p <= n) minp[i*p] = p;
@@ -9,23 +9,21 @@ struct factorizer {
 		}
 	}
 	
-	auto factorize(int x) const {
+	auto factorization(int x) const {
 		static pair<int,int> f[11], *e;
 		for(e = f; x>1; ++e) {
-			int p = minp[x], q = 0;
-			do ++q, x/=p; while(minp[x]==p);
-			*e = {p, q};
+			auto &[p, q] = *e = {minp[x], 0};
+			do ++q, x/=p; while(minp[x] == p);
 		}
-		return vector<pair<int,int>>(f, e);
+		return vector(f, e);
 	}
 	
 	auto divisors(int x) const {
 		static int divs[1500], dn;
-		divs[0] = dn = 1;
-		for(int p=0,l=0; x>1; x/=p) {
-			if(int c=minp[x]; c!=p) p = c, l = 0;
+		for(int p=0,l=dn=divs[0]=1; x>1; x/=p) {
+			if(p != minp[x]) p = minp[x], l = 0;
 			for(int r=dn; l<r; ) divs[dn++] = divs[l++] * p;
 		}
-		return vector<int>(divs, divs+dn);
+		return vector(divs, divs+dn);
 	}
 };
