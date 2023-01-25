cd3402c7d75702304ecc5968cdecaa10d6400a8c
demidenko <devit15@yandex.ru>
2023-01-25 12:44:44 +0700

    generalize

--- well-known/area_of_union_of_rectangles.cpp
+++ well-known/area_of_union_of_rectangles.cpp
@@ -1,27 +1,29 @@
 //requires @aggregator_range_operation
-int64_t area_of_union_of_rectangles(
+template<class S, class D = int, class P>
+S area_of_union_of_rectangles(
 	//[ax, ay, bx, by] all inclusive
-	const vector<tuple<int,int,int,int>> &rects
+	const vector<tuple<P,P,P,P>> &rects
 ) {
 	struct add_op {
-		int a;
-		add_op(int add): a(add) {}
-		add_op(const add_op &x, const add_op &y): a(x.a+y.a) {}
-		decltype(auto) slice(size_t start, size_t end) const { return *this; }
+		D a;
+		add_op(D add): a{add} {}
+		add_op(const add_op &x, const add_op &y): a{x.a+y.a} {}
+		decltype(auto) slice(size_t, size_t) const { return *this; }
 	};
 	
-	struct icnt {
-		int x, cnt;
-		icnt(): icnt(0,0) {}
-		icnt(int val, int cnt = 1): x(val), cnt(cnt) {}
-		icnt(const icnt &a, const icnt &b): x(min(a.x,b.x)), cnt(0) {
-			if(a.x == x) cnt += a.cnt;
-			if(b.x == x) cnt += b.cnt;
+	struct min_cnt {
+		D m;
+		P cnt;
+		min_cnt(): min_cnt(0,0) {}
+		min_cnt(D val, P cnt): m{val}, cnt{cnt} {}
+		min_cnt(const min_cnt &a, const min_cnt &b): m{min(a.m,b.m)}, cnt{0} {
+			if(a.m == m) cnt += a.cnt;
+			if(b.m == m) cnt += b.cnt;
 		}
-		void apply(const add_op &o, size_t) { x += o.a; }
+		void apply(const add_op &o, size_t) { m += o.a; }
 	};
 	
-	vector<int> ys;
+	vector<P> ys;
 	for(auto [ax, ay, bx, by] : rects) {
 		assert(ax <= bx && ay <= by);
 		ys.push_back(ay);
@@ -30,14 +32,13 @@ int64_t area_of_union_of_rectangles(
 	if(empty(ys)) return 0;
 	
 	sort(begin(ys), end(ys));
-	ys.resize(unique(begin(ys), end(ys)) - begin(ys));
-	const int H = ys.back() - ys[0];
+	ys.erase(unique(begin(ys), end(ys)), end(ys));
 	
-	aggregator<icnt,add_op> t(size(ys)-1, [&](size_t i) {
-		return icnt{0, ys[i+1]-ys[i]};
+	aggregator<min_cnt,add_op> t(size(ys)-1, [&](size_t i) {
+		return min_cnt(0, ys[i+1]-ys[i]);
 	});
 	
-	vector<tuple<int,int,size_t,size_t>> events;
+	vector<tuple<P,int,size_t,size_t>> events;
 	events.reserve(size(rects)*2);
 	for(auto [ax, ay, bx, by] : rects) {
 		size_t sl = lower_bound(begin(ys), end(ys), ay) - begin(ys);
@@ -49,12 +50,13 @@ int64_t area_of_union_of_rectangles(
 		return get<0>(e1) < get<0>(e2);
 	});
 	
-	int64_t area = 0, lx = 0;
+	S area = 0; 
+	P H = ys.back() - ys[0], px = 0;
 	for(auto [x, sig, sl, sr] : events) {
 		auto &cur = t();
-		area += (x - lx) * (cur.x == 0 ? H - cur.cnt : H);
-		t.apply(sl, sr, add_op{sig});
-		lx = x;
+		area += S(x - px) * S(cur.m == 0 ? H - cur.cnt : H);
+		t.apply(sl, sr, sig);
+		px = x;
 	}
 	
 	return area;
