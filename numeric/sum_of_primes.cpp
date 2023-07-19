76574acfa2d2a365fd38a88a4ac3ed4a139b47e1
demidenko <devit15@yandex.ru>
2023-07-20 03:46:30 +0700

    speedup + cleanup

--- numeric/sum_of_primes.cpp
+++ numeric/sum_of_primes.cpp
@@ -1,10 +1,7 @@
 template<class T, size_t K>
 struct sum_of_primes {
-	
-	vector<int32_t> pr;
-	sum_of_primes(int64_t n): pr(get_primes(root2(n+1)*2)) {
-		mem_pi.resize(pr.back() + 1);
-		for(int32_t p : pr) mem_pi[p] = pow<K>(p);
+	sum_of_primes(uint64_t n): pr(get_primes(root2(n+1)*2)), mem_pi(pr.back()+1) {
+		for(auto p : pr) mem_pi[p] = pow<K>(p);
 		for(size_t i=1; i<size(mem_pi); ++i) mem_pi[i] += mem_pi[i-1];
 		
 		size_t bufn = min<size_t>(1 << 17, n);
@@ -17,54 +14,31 @@ struct sum_of_primes {
 		}
 	}
 	
-	///sum of i**K for 1 <= i <= n where all prime factors of i >= pr[m]
-	vector<vector<T>> mem_f;
-	T f(int64_t n, size_t m) {
+	///sum of i**K for 1 <= i <= n where all prime factors of i is >= pr[m]
+	T f(uint64_t n, size_t m) const {
 		if(m == 0) return sum_pows<K>(n);
 		if(m < size(mem_f) && n < size(mem_f[m])) return mem_f[m][n];
-		if(int64_t p = pr[m]; p > n) return 1; //1**K
+		if(uint64_t p = pr[m]; p > n) return 1; //1**K
 		else if(p * p > n) return pi(n) - pi(p - 1) + 1;
 		return f(n, m - 1) - f(n / pr[m-1], m - 1) * pow<K>(pr[m-1]);
 	}
 	
 	//sum of p**K for prime numbers 1 <= p <= n
-	vector<T> mem_pi;
-	T pi(int64_t n) {
+	T pi(uint64_t n) const {
 		if(n < 2) return 0;
 		if(n <= pr.back()) return mem_pi[n];
 		const size_t m = upper_bound(begin(pr), end(pr), root3(n)) - begin(pr);
 		T res = f(n, m) + pi(pr[m-1]) - 1;
 		for(size_t k = m; k < size(pr); ++k)
-			if(int64_t p = pr[k]; p * p > n) break ;
+			if(uint64_t p = pr[k]; p * p > n) break ;
 			else res -= (pi(n / p) - pi(p - 1)) * pow<K>(p);
 		return res;
 	}
 	
-	static vector<int> get_primes(int32_t n) {
-		vector<int32_t> md(n + 1), pr;
-		for(int32_t i = 2; i <= n; ++i) {
-			if(md[i] == 0) pr.push_back(md[i] = i);
-			for(int32_t p : pr) {
-				if(p > md[i] || p * i > n) break ;
-				md[p * i] = p;
-			}
-		}
-		return pr;
-	}
+	static inline uint64_t root2(uint64_t x) { return sqrtl(x); }
+	static inline uint64_t root3(uint64_t x) { return cbrtl(x); }
 	
-	static int32_t root2(int64_t x) {
-		int64_t s = sqrt(x);
-		while(s * s <= x) ++s;
-		return s - 1;
-	}
-	
-	static int32_t root3(int64_t x) {
-		int64_t s = cbrt(x);
-		while(s * s * s <= x) ++s;
-		return s - 1;
-	}
-	
-	template<size_t P> static inline T pow(int64_t n) {
+	template<size_t P> static inline T pow(uint64_t n) {
 		if constexpr (P == 0) return 1; else
 		if constexpr (P %2 == 1) return pow<P-1>(n) * T(n); 
 		else {
@@ -73,16 +47,16 @@ struct sum_of_primes {
 		}
 	}
 	
-	template<size_t P> static T sum_pows(int64_t n) {
+	template<size_t P> static T sum_pows(uint64_t n) {
 		static_assert(P <= 3);
 		if constexpr (P == 0) return n; else
 		if constexpr (P == 1) {
-			int64_t a = n, b = n + 1;
+			uint64_t a = n, b = n + 1;
 			if(a %2 == 0) a /= 2; else b /= 2;
 			return T(a) * T(b);
 		} else
 		if constexpr (P == 2) {
-			int64_t a = n, b = n + 1, c = 2 * n + 1;
+			uint64_t a = n, b = n + 1, c = 2 * n + 1;
 			if(a %2 == 0) a /= 2; else b /= 2;
 			if(a %3 == 0) a /= 3; else if(b %3 == 0) b /= 3; else c /= 3;
 			return T(a) * T(b) * T(c);
@@ -92,4 +66,23 @@ struct sum_of_primes {
 			return s1 * s1;
 		}
 	}
+	
+	private:
+	vector<uint32_t> pr;
+	vector<T> mem_pi;
+	vector<vector<T>> mem_f;
+	
+	static auto get_primes(uint32_t n) {
+		vector<uint32_t> md(n + 1), pr;
+		for(uint32_t i = 2; i <= n; ++i) {
+			if(md[i] == 0) pr.push_back(md[i] = i);
+			for(uint32_t p : pr) {
+				if(p > md[i] || p * i > n) break ;
+				md[p * i] = p;
+			}
+		}
+		return pr;
+	}
 };
+
+using count_of_primes = sum_of_primes<uint64_t, 0>;
