b8cb03d31268c8aa0e4963f0535d66f2b8b3de3f
demidenko <devit15@yandex.ru>
2024-03-04 21:49:48 +0700

    rename

--- numeric/factors.cpp
+++ numeric/factors.cpp
@@ -2,7 +2,7 @@ using uint128_t = __uint128_t;
 template<class T> using square_t = conditional_t<sizeof(T) < sizeof(uint64_t), uint64_t, uint128_t>;
 
 template<uint32_t... A, std::unsigned_integral T>
-bool miller_rabin(T n) {
+bool __miller_rabin(T n) {
 	assert(n > 1 && n % 2 == 1);
 	auto test = [n, z=std::countr_zero(n-1)](square_t<T> a) {
 		square_t<T> r = 1; 
@@ -20,16 +20,16 @@ bool is_prime(T n) {
 	if constexpr (is_signed_v<T>) return is_prime<make_unsigned_t<T>>(n);
 	else if constexpr (sizeof(T) > sizeof(uint32_t)) {
 		if(n <= numeric_limits<uint32_t>::max()) return is_prime<uint32_t>(n);
-		return miller_rabin<2, 325, 9375, 28178, 450775, 9780504, 1795265022>(n);
-	} else return miller_rabin<2, 7, 61>(n);
+		return __miller_rabin<2, 325, 9375, 28178, 450775, 9780504, 1795265022>(n);
+	} else return __miller_rabin<2, 7, 61>(n);
 }
 
 template<std::integral T>
-void pollard(T n, auto &p) {
+void __get_factors(T n, auto &p) {
 	assert(n > 0 && n % 2 == 1);
-	if constexpr (is_signed_v<T>) return pollard<make_unsigned_t<T>>(n, p);
+	if constexpr (is_signed_v<T>) return __get_factors<make_unsigned_t<T>>(n, p);
 	if constexpr (sizeof(T) > sizeof(uint32_t)) {
-		if(n <= numeric_limits<uint32_t>::max()) return pollard<uint32_t>(n, p);
+		if(n <= numeric_limits<uint32_t>::max()) return __get_factors<uint32_t>(n, p);
 	}
 	
 	if(n == 1) return ;
@@ -40,7 +40,7 @@ void pollard(T n, auto &p) {
 		auto &&g = [n, a=rnd()%(n-1)+1](square_t<T> x) { return (x*x + a) %n; };
 		for(T x = rnd()%(n-3)+3, y = g(x); x != y; x=g(x), y=g(g(y)))
 		if(const T d = std::gcd(x<y ? y-x : x-y, n); d > 1)
-			return pollard<T>(d, p), pollard<T>(n / d, p);
+			return __get_factors<T>(d, p), __get_factors<T>(n / d, p);
 	}
 }
 
@@ -51,6 +51,6 @@ vector<T> factors(T n) {
 	while(n%2 == 0) n/=2, f.push_back(2);
 	for(T p = 3; p <= 37 && p*p <= n; p += 2) 
 		while(n%p == 0) n/=p, f.push_back(p);
-	pollard(n, f);
+	__get_factors(n, f);
 	return f;
 }
