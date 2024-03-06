68b2d115cf42cc9730266972183a20df572767c1
demidenko <devit15@yandex.ru>
2024-03-07 03:58:18 +0700

    upd to c++20

--- misc/nearest.cpp
+++ misc/nearest.cpp
@@ -1,8 +1,7 @@
-template<template<class> class C, int dir, class index_t = ptrdiff_t, class T>
-vector<index_t> nearest(const vector<T> &v) {
-	static_assert(abs(dir) == 1 && is_signed_v<index_t>);
+template<template<class> class C, int dir, std::signed_integral index_t = ptrdiff_t, class T>
+vector<index_t> nearest(const vector<T> &v) requires(std::abs(dir) == 1) {
 	static constexpr C<T> cmp{};
-	const index_t n = size(v), out = dir < 0 ? -1 : n;
+	const index_t n = ssize(v), out = dir < 0 ? -1 : n;
 	vector<index_t> f(n);
 	for(index_t i = out-dir, k = 0; k < n; ++k, i-=dir)
 		for(f[i] = i+dir; f[i]!=out && !cmp(v[f[i]], v[i]); f[i] = f[f[i]]) ;
