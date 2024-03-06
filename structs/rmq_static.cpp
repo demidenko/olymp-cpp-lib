c8838263db4754032bb5a5bf3ad6f1e32c89a087
demidenko <devit15@yandex.ru>
2024-03-06 21:05:56 +0700

    use c++20; make empty if empty;

--- structs/rmq_static.cpp
+++ structs/rmq_static.cpp
@@ -1,8 +1,8 @@
 template<class T, const T& f(const T&, const T&) = std::min<T> >
 struct rmq {
 	rmq() {}
-	rmq(const vector<T> &vals): t(empty(vals) ? 1 : __lg(size(vals)) + 1) {
-		t[0] = vals;
+	rmq(const vector<T> &vals): t(std::bit_width(size(vals))) {
+		if(!empty(t)) t[0] = vals;
 		for(size_t k=1, p=1; k<size(t); ++k, p<<=1) {
 			auto &a = t[k], &b = t[k-1];
 			a.resize(size(vals)-p*2+1);
@@ -10,8 +10,8 @@ struct rmq {
 		}
 	}
 	const T& operator()(size_t l, size_t r) const {
-		auto h = __lg(r-l);
-		return f(t[h][l], t[h][r-(size_t(1)<<h)]);
+		auto h = std::bit_width(r-l) - 1;
+		return f(t[h][l], t[h][r-(1uz<<h)]);
 	}
 	private: vector<vector<T>> t;
 };
