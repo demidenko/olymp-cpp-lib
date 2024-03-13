9701d8d5e191d8811669c65d5496e46e2fd3e0f0
demidenko <devit15@yandex.ru>
2024-03-13 17:30:55 +0700

    build + kth_jump

--- alpha/binary_lifter.cpp
+++ alpha/binary_lifter.cpp
@@ -1,19 +1,63 @@
 struct binary_lifter {
-	constexpr static size_t none = -1;
-	
-	binary_lifter(): binary_lifter(0) {}
-	
-	binary_lifter(const std::ranges::sized_range auto &p): binary_lifter(size(p)) {
-		const size_t n = size(p);
-		std::ranges::copy(p, begin(jump[0]));
-		for(size_t t : jump[0]) assert(t == none || t < n);
-		//TODO: support functional graphs, not trees only
+	binary_lifter(const std::ranges::sized_range auto &to = std::ranges::empty_view<size_t>{}) {
+		const size_t n = size(to);
+		vector<size_t> p(n+1, n);
+		std::ranges::copy(to, begin(p));
+		assert(*std::ranges::max_element(p) <= n);
+		
+		jumps_to_cyc.resize(n+1);
+		cyc_pos.resize(n+1);
+		
+		vector<bool> used(n+1);
+		vector<size_t> used_at(n+1, n);
+		for(size_t i=0; i<=n; ++i) if(!used[i]) {
+			vector<size_t> path;
+			for(size_t v=i; ; ) {
+				path.push_back(v);
+				size_t t = v < n ? p[v] : n;
+				if(used[t]) {
+					if(used_at[t] == i) {
+						auto it = find(begin(path), end(path), t);
+						auto &cyc = cycs.emplace_back(it, end(path));
+						for(size_t pos=0; pos<size(cyc); ++pos) {
+							size_t x = cyc[pos];
+							jumps_to_cyc[x] = {0, x};
+							cyc_pos[x] = {size(cycs) - 1, pos};
+						}
+						path.erase(it, end(path));
+					}
+					break ;
+				}
+				used[v] = true;
+				used_at[v] = i;
+				v = t;
+			}
+			for(size_t x : std::views::reverse(path))
+				(jumps_to_cyc[x] = jumps_to_cyc[p[x]]).first++;
+		}
+		
+		size_t d = std::ranges::max_element(jumps_to_cyc)->first;
+		size_t L = std::countr_zero(std::bit_ceil(d));
+		if(!L) return ;
+		jump.assign(L, vector(n+1, n));
+		jump[0] = std::move(p);
 		for(size_t h=1; h<L; ++h)
-		for(size_t i=0; i<n; ++i)
-			if(size_t j=jump[h-1][i]; j!=none) jump[h][i] = jump[h-1][j];
+		for(size_t i=0; i<n; ++i) jump[h][i] = jump[h-1][jump[h-1][i]];
 	}
 	
-	struct result { size_t first_false, last_true, jumps_to_false; };
+	size_t kth_jump(size_t i, std::unsigned_integral auto k) const {
+		if(auto [d, v] = jumps_to_cyc[i]; k < d) {
+			for(size_t h=0; k>0; ++h, k>>=1) if(k&1) i = jump[h][i];
+			return i;
+		} else {
+			k -= d;
+			auto [row, pos] = cyc_pos[v];
+			auto &cyc = cycs[row];
+			return cyc[(pos + k) % size(cyc)];
+		}
+	}
+	
+	/*struct result { size_t first_false, last_true, jumps_to_false; };
 	result first_false(size_t i, auto &&pred) const {
 		if(!pred(i)) return { i, none, 0 };
 		size_t jumps = 0;
@@ -23,20 +67,11 @@ struct binary_lifter {
 			jumps += size_t(1) << h;
 		}
 		return { jump[0][i], i, jumps+1 };
-	}
-	
-	size_t kth_jump(size_t i, size_t k) const {
-		if(k >= (size_t(1) << L)) return none;
-		for(size_t h=0; k>0 && i!=none; ++h, k>>=1) if(k&1) i = jump[h][i];
-		return i; 
-	}
+	}*/
 	
 	private:
-	size_t L;
 	vector<vector<size_t>> jump;
-	
-	binary_lifter(size_t n) {
-		for(L = 1; (size_t(1)<<L) < n; ++L);
-		jump.assign(L, vector(n, none));
-	}
+	vector<vector<size_t>> cycs;
+	vector<pair<size_t,size_t>> cyc_pos;
+	vector<pair<size_t,size_t>> jumps_to_cyc;
 };
