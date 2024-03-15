693af8f6448d77cff24dc717d8db0a342aca5a26
demidenko <devit15@yandex.ru>
2024-03-15 22:24:44 +0700

    code ref

--- alpha/binary_lifter.cpp
+++ alpha/binary_lifter.cpp
@@ -1,59 +1,59 @@
 struct binary_lifter {
-	binary_lifter(const std::ranges::sized_range auto &to = std::ranges::empty_view<size_t>{}) {
-		const size_t n = size(to);
-		vector<size_t> p(n+1, n);
-		std::ranges::copy(to, begin(p));
-		assert(*std::ranges::max_element(p) <= n);
+	binary_lifter(const std::ranges::sized_range auto &f = std::ranges::empty_view<size_t>{}) {
+		const size_t n = size(f);
+		if(n == 0) return ;
 		
-		jumps_to_cyc.resize(n+1);
-		cyc_pos.resize(n+1);
+		const vector<size_t> p(begin(f), end(f));
+		assert(*std::ranges::max_element(p) < n);
 		
-		vector<bool> used(n+1);
-		vector<size_t> used_at(n+1, n);
-		for(size_t i=0; i<=n; ++i) if(!used[i]) {
+		jumps_to_cyc.assign(n, 0);
+		first_cyc_v.resize(n);
+		cyc_pos.resize(n);
+		vector<size_t> used(n, n);
+		for(size_t i=0; i<n; ++i) if(used[i] == n) {
 			vector<size_t> path;
 			for(size_t v=i; ; ) {
+				used[v] = i;
 				path.push_back(v);
-				size_t t = v < n ? p[v] : n;
-				if(used[t]) {
-					if(used_at[t] == i) {
-						auto it = find(begin(path), end(path), t);
+				v = p[v];
+				if(used[v] <= i) {
+					if(used[v] == i) {
+						auto it = find(begin(path), end(path), v);
 						auto &cyc = cycs.emplace_back(it, end(path));
 						for(size_t pos=0; pos<size(cyc); ++pos) {
 							size_t x = cyc[pos];
-							jumps_to_cyc[x] = {0, x};
+							first_cyc_v[x] = x;
 							cyc_pos[x] = {size(cycs) - 1, pos};
 						}
 						path.erase(it, end(path));
 					}
 					break ;
 				}
-				used[v] = true;
-				used_at[v] = i;
-				v = t;
+				used[v] = i;
 			}
-			for(size_t x : std::views::reverse(path))
-				(jumps_to_cyc[x] = jumps_to_cyc[p[x]]).first++;
+			for(size_t x : std::views::reverse(path)) {
+				first_cyc_v[x] = first_cyc_v[p[x]];
+				jumps_to_cyc[x] = jumps_to_cyc[p[x]] + 1;
+			}
 		}
 		
-		size_t d = std::ranges::max_element(jumps_to_cyc)->first;
+		size_t d = *std::ranges::max_element(jumps_to_cyc);
 		size_t L = std::countr_zero(std::bit_ceil(d));
-		if(!L) return ;
-		jump.assign(L, vector(n+1, n));
+		if(L == 0) return ;
+		jump.assign(L, vector(n, n));
 		jump[0] = std::move(p);
 		for(size_t h=1; h<L; ++h)
 		for(size_t i=0; i<n; ++i) jump[h][i] = jump[h-1][jump[h-1][i]];
 	}
 	
 	size_t kth_jump(size_t i, std::unsigned_integral auto k) const {
-		if(auto [d, v] = jumps_to_cyc[i]; k < d) {
+		if(size_t d = jumps_to_cyc[i]; k < d) {
 			for(size_t h=0; k>0; ++h, k>>=1) if(k&1) i = jump[h][i];
 			return i;
 		} else {
-			k -= d;
-			auto [row, pos] = cyc_pos[v];
+			auto [row, pos] = cyc_pos[first_cyc_v[i]];
 			auto &cyc = cycs[row];
-			return cyc[(pos + k) % size(cyc)];
+			return cyc[(pos + (k - d)) % size(cyc)];
 		}
 	}
 	
@@ -73,5 +73,5 @@ struct binary_lifter {
 	vector<vector<size_t>> jump;
 	vector<vector<size_t>> cycs;
 	vector<pair<size_t,size_t>> cyc_pos;
-	vector<pair<size_t,size_t>> jumps_to_cyc;
+	vector<size_t> jumps_to_cyc, first_cyc_v;
 };
