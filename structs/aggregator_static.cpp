9b39ffc8de1987bccf3b7fdd23cf32f8e851f44e
demidenko <devit15@yandex.ru>
2024-03-17 22:46:33 +0700

    upd to c++20

--- structs/aggregator_static.cpp
+++ structs/aggregator_static.cpp
@@ -1,6 +1,6 @@
 template<class T>
 struct aggregator_static {
-	aggregator_static(const vector<auto> &vals): t(empty(vals) ? 1 : __lg(size(vals)) + 1) {
+	aggregator_static(const auto &vals): t(empty(vals) ? 1 : std::bit_width(size(vals))) {
 		t[0].assign(begin(vals), end(vals));
 		t[0].resize(size_t(1) << size(t));
 		for(size_t p=4, k=1; k<size(t); ++k, p<<=1) {
@@ -14,8 +14,8 @@ struct aggregator_static {
 	T operator()(size_t l, size_t r) const {
 		if(l >= r) return T{};
 		if(l == --r) return t[0][l];
-		size_t h = __lg(l^r);
-		return T(t[h][l], t[h][r]);
+		auto &row = t[std::bit_width(l^r)-1];
+		return T(row[l], row[r]);
 	}
 	private: vector<vector<T>> t;
 };
