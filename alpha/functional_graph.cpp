fd9f7003ae73fac47f67dbc3532f5acd86703880
demidenko <devit15@yandex.ru>
2024-03-17 16:24:01 +0700

    reduce data

--- alpha/functional_graph.cpp
+++ alpha/functional_graph.cpp
@@ -1,20 +1,19 @@
 struct func_graph {
-	func_graph(const std::ranges::sized_range auto &f = std::ranges::empty_view<size_t>{}) {
-		const size_t n = size(f);
-		if(n == 0) return ;
-		
+	func_graph(const std::ranges::range auto &f = std::ranges::empty_view<size_t>{}) {
 		const vector<size_t> p(begin(f), end(f));
+		const size_t n = size(p);
+		if(n == 0) return ; //nothing bad just skip overhead
 		assert(*std::ranges::max_element(p) < n);
 		
 		size_t vn = 0;
-		vs.resize(n);
+		vs_pos.resize(n);
 		
 		size_t gn = 0, g_none = -1;
-		vector<size_t> g_id(n), g_par(n);
+		vector<size_t> g_id(n), g_par(n), ts;
 		
 		jump.resize(n);
 		jump_len.resize(n);
-		on_cyc.assign(n, false);
+		vector<bool> on_cyc(n, false);
 		vector<size_t> used(n, n);
 		for(size_t i=0; i<n; ++i) if(used[i] == n) {
 			vector<size_t> path;
@@ -27,7 +26,7 @@ struct func_graph {
 						const auto itv = find(begin(path), end(path), v);
 						for(auto it = itv; it != end(path); ++it) {
 							size_t x = *it;
-							vs[vn++] = x;
+							vs_pos[x] = vn++;
 							jump[x] = v;
 							jump_len[x] = end(path) - it;
 							on_cyc[x] = true;
@@ -41,6 +40,7 @@ struct func_graph {
 			for(size_t x : std::views::reverse(path)) {
 				g_id[x] = gn++;
 				g_par[g_id[x]] = on_cyc[p[x]] ? g_none : g_id[p[x]];
+				ts.push_back(x);
 			}
 		}
 		
@@ -48,12 +48,9 @@ struct func_graph {
 		std::ranges::replace(g_par, g_none, gn);
 		for(size_t i=0; i<gn; ++i) g[g_par[i]].push_back(i);
 		heavy_light_decomposition hld(g, gn);
-		for(size_t i=0; i<n; ++i) if(!on_cyc[i]) vs[n - hld.index(g_id[i])] = i;
-		vs_pos.resize(n);
-		for(size_t i=0; i<n; ++i) vs_pos[vs[i]] = i;
-		for(size_t i=n-1; i>=vn; --i) {
-			size_t v = vs[i];
-			if(vs_pos[p[v]] == i + 1) {
+		for(size_t i=0; i<n; ++i) if(!on_cyc[i]) vs_pos[i] = n - hld.index(g_id[i]);
+		for(size_t v : ts) {
+			if(index(p[v]) == index(v) + 1) {
 				jump[v] = jump[p[v]];
 				jump_len[v] = jump_len[p[v]] + 1;
 			} else {
@@ -69,23 +66,12 @@ struct func_graph {
 		vector<tuple<size_t,size_t,bool>> res;
 		for(;; v = jump[v]) {
 			size_t l = index(v);
-			bool last = on_cyc[v] && v == jump[v];
+			bool last = v == jump[v];
 			res.emplace_back(l, l + jump_len[v], last);
 			if(last) return res;
 		}
 	}
 	
-	size_t kth_jump(size_t i, std::unsigned_integral auto k) const {
-		for(auto [l, r, last] : decompose(i)) {
-			if(last) k %= r - l;
-			if(r - l > k) { i = vs[l + k]; break; }
-			k -= r - l;
-		}
-		return i;
-	}
-	
 	private:
-	vector<size_t> vs, vs_pos;
-	vector<size_t> jump, jump_len;
-	vector<bool> on_cyc;
+	vector<size_t> vs_pos, jump, jump_len;
 };
