9070f87d7e5ecd19afbe2c623da827a40b9fb9ac
demidenko <devit15@yandex.ru>
2024-03-29 06:48:20 +0700

    code ref

--- structs/rsq_range_assign.cpp
+++ structs/rsq_range_assign.cpp
@@ -4,18 +4,16 @@ struct rsq_range_assign {
 	
 	rsq_range_assign(const vector<auto> &vals): rsq_range_assign(size(vals)) {
 		for(size_t i=0; i<size(vals); ++i) t[i+d].first = vals[i];
-		_build();
+		for(size_t i=d; i-->1; ) t[i].first = t[i*2].first + t[i*2+1].first;
 	}
 	
 	void assign(size_t l, size_t r, const A &value) {
-		if(r>d) r = d;
-		if(l<r) _assign(l,r,value,0,d,1);
+		if(l < r) assign(l, r, value, 0, d, 1);
 	}
 	
 	T operator()(size_t l, size_t r) const {
-		if(r>d) r = d;
 		T buf{};
-		if(l<r) _calc(l,r,0,d,1,buf);
+		if(l < r) calc(l, r, 0, d, 1, buf);
 		return buf;
 	}
 	
@@ -24,34 +22,28 @@ struct rsq_range_assign {
 	vector<pair<T,optional<A>>> t;
 	
 	static size_t _p2(size_t n) { return n > 1 ? (2<<__lg(n-1)) : 1; }
-	void _build() { for(size_t i=d; i-->1; ) _build_node(i); }
-	inline void _build_node(size_t v) { t[v].first = t[v*2].first + t[v*2+1].first; }
 	
-	void _push(size_t v, size_t len) {
-		if(auto &a = t[v].second) {
-			t[v*2] = t[v*2+1] = {T(len>>1) * *a, a};
-			a.reset();
-		}
-	}
-	
-	void _assign(size_t i, size_t j, const A &val, size_t l, size_t r, size_t v) {
-		if(i==l && j==r) {
+	void assign(size_t i, size_t j, const A &val, size_t l, size_t r, size_t v) {
+		if(i == l && j == r) {
 			t[v] = {T(r-l) * val, val};
 			return ;
 		}
-		_push(v,r-l);
 		size_t m = (l+r)>>1;
-		if(i<m) _assign(i,min(j,m),val,l,m,v*2);
-		if(m<j) _assign(max(i,m),j,val,m,r,v*2+1);
-		_build_node(v);
+		if(auto &a = t[v].second) {
+			t[v*2] = t[v*2+1] = {T(m-l) * *a, a};
+			a.reset();
+		}
+		if(i < m) assign(i, min(j,m), val, l, m, v*2);
+		if(m < j) assign(max(i,m), j, val, m, r, v*2+1);
+		t[v].first = t[v*2].first + t[v*2+1].first;
 	}
 	
-	void _calc(size_t i, size_t j, size_t l, size_t r, size_t v, T &buf) const {
-		if(i==l && j==r) buf += t[v].first; else
+	void calc(size_t i, size_t j, size_t l, size_t r, size_t v, T &buf) const {
+		if(i == l && j == r) buf += t[v].first; else
 		if(auto a = t[v].second) buf += T(j-i) * *a; else {
 			size_t m = (l+r)>>1;
-			if(i<m) _calc(i,min(j,m),l,m,v*2,buf);
-			if(m<j) _calc(max(i,m),j,m,r,v*2+1,buf);
+			if(i < m) calc(i, min(j,m), l, m, v*2, buf);
+			if(m < j) calc(max(i,m), j, m, r, v*2+1, buf);
 		}
 	}
 };
