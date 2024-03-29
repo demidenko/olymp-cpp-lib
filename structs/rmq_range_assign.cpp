96fb8916bf4393f0f0a35b0d3d1afbe9a60a784d
demidenko <devit15@yandex.ru>
2024-03-29 22:04:37 +0700

    use comparator instead of min/max

--- structs/rmq_range_assign.cpp
+++ structs/rmq_range_assign.cpp
@@ -1,10 +1,10 @@
-template<class T, const T& f(const T&, const T&) = std::min<T> >
+template<class T, auto cmp = std::less<T>{}>
 struct rmq_range_assign {
 	explicit rmq_range_assign(size_t sz = 0): d(std::bit_ceil(sz)), t(d*2) {}
 	
 	rmq_range_assign(const std::ranges::range auto &vals): rmq_range_assign(size(vals)) {
 		for(auto it=begin(t)+d; auto &&val : vals) it++->first = val;
-		for(size_t i=d; i-->1; ) t[i].first = f(t[i*2].first, t[i*2+1].first);
+		for(size_t i=d; i-->1; ) t[i].first = std::min(t[i*2].first, t[i*2+1].first, cmp);
 	}
 	
 	void assign(size_t l, size_t r, const T &value) {
@@ -13,7 +13,7 @@ struct rmq_range_assign {
 	
 	T operator()(size_t l, size_t r) const {
 		assert(l < r);
-		return calc(l, r, 0, d, 1);
+		return min(l, r, 0, d, 1);
 	}
 	
 	T operator[](size_t i) const {
@@ -38,13 +38,13 @@ struct rmq_range_assign {
 		size_t m = std::midpoint(l, r);
 		if(i < m) assign(i, std::min(j,m), val, l, m, v*2);
 		if(m < j) assign(std::max(i,m), j, val, m, r, v*2+1);
-		t[v].first = f(t[v*2].first, t[v*2+1].first);
+		t[v].first = std::min(t[v*2].first, t[v*2+1].first, cmp);
 	}
 	
-	T calc(size_t i, size_t j, size_t l, size_t r, size_t v) const {
+	T min(size_t i, size_t j, size_t l, size_t r, size_t v) const {
 		if((i == l && j == r) || t[v].second) return t[v].first;
-		if(size_t m = std::midpoint(l, r); j <= m) return calc(i, j, l, m, v*2);
-		else if(i >= m) return calc(i, j, m, r, v*2+1);
-		else return f(calc(i, m, l, m, v*2), calc(m, j, m, r, v*2+1));
+		if(size_t m = std::midpoint(l, r); j <= m) return min(i, j, l, m, v*2);
+		else if(i >= m) return min(i, j, m, r, v*2+1);
+		else return std::min(min(i, m, l, m, v*2), min(m, j, m, r, v*2+1), cmp);
 	}
 };
