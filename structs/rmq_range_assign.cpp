d56553bf6d6c2fe33313a02a2f2c9b0df7f3f57a
demidenko <devit15@yandex.ru>
2024-03-29 21:56:12 +0700

    remove neutral + upd code

--- structs/rmq_range_assign.cpp
+++ structs/rmq_range_assign.cpp
@@ -1,25 +1,21 @@
 template<class T, const T& f(const T&, const T&) = std::min<T> >
 struct rmq_range_assign {
-	constexpr static T neutral = [](T m,T M){return f(m,M)==m?M:m;}(numeric_limits<T>::min(),numeric_limits<T>::max());
+	explicit rmq_range_assign(size_t sz = 0): d(std::bit_ceil(sz)), t(d*2) {}
 	
-	explicit rmq_range_assign(size_t sz = 0): d(_p2(sz)), t(d*2, {neutral, false}) {}
-	
-	rmq_range_assign(const vector<auto> &vals): rmq_range_assign(size(vals)) {
-		for(size_t i=0; i<size(vals); ++i) t[i+d].first = vals[i];
-		_build();
+	rmq_range_assign(const std::ranges::range auto &vals): rmq_range_assign(size(vals)) {
+		for(auto it=begin(t)+d; auto &&val : vals) it++->first = val;
+		for(size_t i=d; i-->1; ) t[i].first = f(t[i*2].first, t[i*2+1].first);
 	}
 	
 	void assign(size_t l, size_t r, const T &value) {
-		if(r>d) r = d;
-		if(l<r) _assign(l,r,value,0,d,1);
+		if(l < r) assign(l, r, value, 0, d, 1);
 	}
 	
 	T operator()(size_t l, size_t r) const {
-		if(r>d) r = d;
-		T buf = neutral;
-		if(l<r) _calc(l,r,0,d,1,buf);
-		return buf;
+		assert(l < r);
+		return calc(l, r, 0, d, 1);
 	}
+	
 	T operator[](size_t i) const {
 		T result = t[i+=d].first;
 		while(i>>=1) if(t[i].second) result = t[i].first;
@@ -30,35 +26,25 @@ struct rmq_range_assign {
 	size_t d;
 	vector<pair<T,bool>> t;
 	
-	static size_t _p2(size_t n) { return n > 1 ? (2<<__lg(n-1)) : 1; }
-	void _build() { for(size_t i=d; i-->1; ) _build_node(i); }
-	inline void _build_node(size_t v) { t[v].first = f(t[v*2].first, t[v*2+1].first); }
-	
-	void _push(size_t v) {
+	void assign(size_t i, size_t j, const T &val, size_t l, size_t r, size_t v) {
+		if(i == l && j == r) {
+			t[v] = {val, true};
+			return ;
+		}
 		if(t[v].second) {
 			t[v*2] = t[v*2+1] = t[v];
 			t[v].second = false;
 		}
-	}
-	
-	void _assign(size_t i, size_t j, const T &val, size_t l, size_t r, size_t v) {
-		if(i==l && j==r) {
-			t[v] = {val, true};
-			return ;
-		}
-		_push(v);
-		size_t m = (l+r)>>1;
-		if(i<m) _assign(i,min(j,m),val,l,m,v*2);
-		if(m<j) _assign(max(i,m),j,val,m,r,v*2+1);
+		size_t m = std::midpoint(l, r);
+		if(i < m) assign(i, std::min(j,m), val, l, m, v*2);
+		if(m < j) assign(std::max(i,m), j, val, m, r, v*2+1);
 		t[v].first = f(t[v*2].first, t[v*2+1].first);
 	}
 	
-	void _calc(size_t i, size_t j, size_t l, size_t r, size_t v, T &buf) const {
-		if(i==l && j==r || t[v].second) buf = f(buf, t[v].first);
-		else {
-			size_t m = (l+r)>>1;
-			if(i<m) _calc(i,min(j,m),l,m,v*2,buf);
-			if(m<j) _calc(max(i,m),j,m,r,v*2+1,buf);
-		}
+	T calc(size_t i, size_t j, size_t l, size_t r, size_t v) const {
+		if((i == l && j == r) || t[v].second) return t[v].first;
+		if(size_t m = std::midpoint(l, r); j <= m) return calc(i, j, l, m, v*2);
+		else if(i >= m) return calc(i, j, m, r, v*2+1);
+		else return f(calc(i, m, l, m, v*2), calc(m, j, m, r, v*2+1));
 	}
 };
