c2e2f5f35586291f73618a8ee2245ed7f54c3669
demidenko <devit15@yandex.ru>
2024-03-29 22:17:19 +0700

    comparator instead of min/max

--- structs/rmq_static.cpp
+++ structs/rmq_static.cpp
@@ -1,4 +1,4 @@
-template<class T, const T& f(const T&, const T&) = std::min<T> >
+template<class T, auto cmp = std::less<T>{}>
 struct rmq {
 	rmq() {}
 	rmq(const vector<T> &vals): t(std::bit_width(size(vals))) {
@@ -6,12 +6,12 @@ struct rmq {
 		for(size_t k=1, p=1; k<size(t); ++k, p<<=1) {
 			auto &a = t[k], &b = t[k-1];
 			a.resize(size(vals)-p*2+1);
-			for(size_t i=0; i<size(a); ++i) a[i] = f(b[i], b[i+p]);
+			for(size_t i=0; i<size(a); ++i) a[i] = std::min(b[i], b[i+p], cmp);
 		}
 	}
 	const T& operator()(size_t l, size_t r) const {
 		auto h = std::bit_width(r-l) - 1;
-		return f(t[h][l], t[h][r-(1uz<<h)]);
+		return std::min(t[h][l], t[h][r-(1uz<<h)], cmp);
 	}
 	private: vector<vector<T>> t;
 };
