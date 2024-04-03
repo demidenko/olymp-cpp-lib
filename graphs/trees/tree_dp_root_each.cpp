66e358e9c6dc67f030d0893c5bd01a42762cb6f7
demidenko <devit15@yandex.ru>
2024-04-03 14:09:41 +0700

    code ref

--- graphs/trees/tree_dp_root_each.cpp
+++ graphs/trees/tree_dp_root_each.cpp
@@ -1,27 +1,24 @@
 template<class S, class ...T>
 vector<S> tree_dp_root_each(
 	const graph_t<T...> &g,
-	auto &&single,
+	auto &&single, //[](size_t v) -> S { ... }
 	auto &&link //[](S dv, S di, auto &edge) -> S { auto [v, i, ...] = edge }
 ) {
+	auto edge_cat = [](size_t v, auto &e) { return tuple_cat(tuple{v}, tuple<size_t,T...>{e}); };
+	
 	const size_t n = size(g);
-	vector<S> res(n), up(n);
-	vector<edge_t<size_t,T...>> epar(n);
-	
-	auto expand_edge = [](size_t v, auto &e) { return tuple_cat(tuple{v}, tuple<size_t,T...>{e}); };
-	
 	vector<size_t> par(n,-1), q(n);
+	vector<edge_t<size_t,T...>> epar(n);
 	for(size_t ql=0,qr=0,st=0; st<n; ++st) if(par[st] == -1)
-		for(q[qr++] = st; ql<qr; ++ql) {
-			size_t v = q[ql];
-			res[v] = single(v);
-			for(auto &edge : g[v]) if(size_t i=edge; i!=par[v]) {
+		for(q[qr++] = st; ql<qr; ++ql)
+			for(size_t v = q[ql]; auto &edge : g[v]) if(size_t i=edge; i!=par[v]) {
 				par[i] = v;
-				epar[i] = expand_edge(v,edge);
+				epar[i] = edge_cat(v, edge);
 				q[qr++] = i;
 			}
-		}
 	
+	vector<S> res(n), up(n);
+	for(size_t v=0; v<n; ++v) res[v] = single(v);
 	for(size_t v : views::reverse(q)) if(size_t p=par[v]; p!=-1) res[p] = link(res[p], res[v], epar[v]);
 	
 	auto cf = [&](const S &x, size_t i) { return link(x, res[i], epar[i]); };
@@ -37,7 +34,7 @@ vector<S> tree_dp_root_each(
 		ar.clear();
 		for(auto &edge : g[v]) if(size_t i=edge; i!=par[v]) ar.push_back(i); else {
 			if(par[i]!=-1) up[v] = link(up[v], up[i], epar[i]);
-			res[v] = link(res[v], up[v], epar[v] = expand_edge(v,edge));
+			res[v] = link(res[v], up[v], epar[v] = edge_cat(v, edge));
 		}
 		if(!empty(ar)) exclude(begin(ar), end(ar), single(v));
 	}
