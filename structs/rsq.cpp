519f4ae022ec0bfda5e055e626c7666d7dba7234
demidenko <devit15@yandex.ru>
2024-04-23 04:47:29 +0700

    classic prefix sum version

--- structs/rsq.cpp
+++ structs/rsq.cpp
@@ -2,18 +2,18 @@ template<class T>
 struct rsq {
 	explicit rsq(size_t n = 0): f(n) {}
 	rsq(const std::ranges::range auto &vals): f(begin(vals), end(vals)) {
-		for(size_t i = size(f); i--;) if(size_t j = i&(i+1)) f[j-1]+=f[i];
+		for(size_t i=0, j, n=size(f); i<n; ++i) if(j=i|i+1; j<n) f[j] += f[i];
 	}
 	void add(size_t i, const T &val) {
-		for(++i; i--; i&=i+1) f[i]+=val;
+		for(; i < size(f); i|=i+1) f[i] += val;
 	}
-	T operator()(size_t l, size_t r) const {
-		return l < r ? sum_suf(l) - sum_suf(r) : T{};
-	}
-	T operator[](size_t i) const { return sum_suf(i) - sum_suf(i+1); }
-	T sum_suf(size_t i) const {
-		T s{}; for(; i<size(f); i|=i+1) s+=f[i];
+	T sum_until(size_t i) const {
+		T s{}; for(; i--; i&=i+1) s += f[i];
 		return s;
 	}
+	T operator()(size_t l, size_t r) const {
+		return l < r ? sum_until(r) - sum_until(l) : T{};
+	}
+	T operator[](size_t i) const { return operator()(i, i+1); }
 	private: vector<T> f;
 };
