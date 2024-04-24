0e63cc5a7d52d3020dd7dea42c5665a2c1fddb7d
demidenko <devit15@yandex.ru>
2024-04-24 08:47:25 +0700

    cdoe update

--- structs/rmq_range_add.cpp
+++ structs/rmq_range_add.cpp
@@ -1,59 +1,47 @@
-template<class T, const T& f(const T&, const T&) = std::min<T>, class A = T >
+template<class T, auto cmp = std::less<T>{}, class A = T >
 struct rmq_range_add {
-	constexpr static T neutral = [](T m,T M){return f(m,M)==m?M:m;}(numeric_limits<T>::min(),numeric_limits<T>::max());
+	explicit rmq_range_add(size_t sz = 0): d(std::bit_ceil(sz)), t(d*2) {}
 	
-	explicit rmq_range_add(size_t sz = 0): d(_p2(sz)), t(d*2, {neutral,{}}) {}
-	
-	rmq_range_add(size_t sz, const T &val): rmq_range_add(sz) {
-		for(size_t i=0; i<sz; ++i) t[i+d].first = val;
-		_build();
+	rmq_range_add(const std::ranges::range auto &vals): rmq_range_add(size(vals)) {
+		for(auto it=begin(t)+d; auto &&val : vals) it++->first = val;
+		for(size_t i=d; i-->1; ) t[i].first = std::min(t[i*2].first, t[i*2+1].first, cmp);
 	}
 	
-	rmq_range_add(const vector<auto> &vals): rmq_range_add(size(vals)) {
-		for(size_t i=0; i<size(vals); ++i) t[i+d].first = vals[i];
-		_build();
+	void add(size_t l, size_t r, const A &val) {
+		if(l < r) add(l, r, val, 0, d, 1);
 	}
 	
 	T operator()(size_t l, size_t r) const {
-		if(r > d) r = d;
-		return l < r ? _calc(l,r,0,d,1) : neutral;
+		assert(l < r);
+		return min(l, r, 0, d, 1);
 	}
-	T operator()() const { return t[1].first; }
+	
 	T operator[](size_t i) const {
 		T result = t[i+=d].first;
 		while(i>>=1) result += t[i].second;
 		return result;
 	}
 	
-	void add(size_t l, size_t r, const A &val) {
-		if(r > d) r = d;
-		if(l < r) _add(l,r,val,0,d,1);
-	}
-	
 	private:
 	size_t d;
 	vector<pair<T,A>> t;
 	
-	static size_t _p2(size_t n) { return n > 1 ? (2<<__lg(n-1)) : 1; }
-	void _build() { for(size_t i=d; i-->1; ) _build_node(i); }
-	inline void _build_node(size_t v) { t[v].first = f(t[v*2].first, t[v*2+1].first) + t[v].second; }
-	
-	void _add(size_t i, size_t j, const A &val, size_t l, size_t r, size_t v) {
-		if(i==l && j==r) {
+	void add(size_t i, size_t j, const A &val, size_t l, size_t r, size_t v) {
+		if(i == l && j == r) {
 			t[v].first += val;
 			t[v].second += val;
 			return ;
 		}
-		size_t m = (l+r)>>1;
-		if(i<m) _add(i,min(j,m),val,l,m,v*2);
-		if(m<j) _add(max(i,m),j,val,m,r,v*2+1);
-		_build_node(v);
+		size_t m = std::midpoint(l, r);
+		if(i < m) add(i, std::min(j,m), val, l, m, v*2);
+		if(m < j) add(std::max(i,m), j, val, m, r, v*2+1);
+		t[v].first = std::min(t[v*2].first, t[v*2+1].first, cmp) + t[v].second;
 	}
 	
-	T _calc(size_t i, size_t j, size_t l, size_t r, size_t v) const {
-		if(i==l && j==r) return t[v].first;
-		size_t m = (l+r)>>1;
-		return (j<=m ? _calc(i,j,l,m,v*2) : i>=m ? _calc(i,j,m,r,v*2+1)
-		 : f(_calc(i,m,l,m,v*2), _calc(m,j,m,r,v*2+1))) + t[v].second;
+	T min(size_t i, size_t j, size_t l, size_t r, size_t v) const {
+		if(i == l && j == r) return t[v].first;
+		size_t m = std::midpoint(l, r);
+		return (j<=m ? min(i, j, l, m, v*2) : i>=m ? min(i, j, m, r, v*2+1)
+		 : std::min(min(i, m, l, m, v*2), min(m, j, m, r, v*2+1))) + t[v].second;
 	}
 };
