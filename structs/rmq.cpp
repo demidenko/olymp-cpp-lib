2cd8ccf63596ebfe8b845858a34dfbf7c47ce65c
demidenko <devit15@yandex.ru>
2024-11-22 20:34:36 +0700

    rmq with cmp

--- structs/rmq.cpp
+++ structs/rmq.cpp
@@ -1,25 +1,27 @@
-template<class T, const T& f(const T&, const T&) = std::min<T> >
+template<class T, auto cmp = std::less<T>{} >
 struct rmq {
-	explicit rmq(size_t n = 0, const T &val = {}): d(n), t(val,d*2) {}
+	explicit rmq(size_t n = 0, const T &val = {}): d(n), t(d*2,val) {}
 	rmq(const vector<T> &vals): d(size(vals)), t(d*2) {
 		copy(begin(vals), end(vals), begin(t)+d);
-		for(size_t i=d; i-->1;) t[i] = f(t[i*2], t[i*2+1]);
+		for(size_t i=d; i-->1;) t[i] = std::min(t[i*2], t[i*2+1], cmp);
 	}
 	void set_value(size_t i, const T &val) {
-		for(t[i+=d]=val; i>1; i>>=1) t[i>>1] = f(t[i], t[i^1]);
+		for(t[i+=d]=val; i>1; i>>=1) t[i>>1] = std::min(t[i], t[i^1], cmp);
 	}
-	void update(size_t i, const T &val) { set_value(i, f(t[i+d],val)); }
+	void update(size_t i, const T &val) { 
+		set_value(i, std::min(t[i+d], val, cmp)); 
+	}
 	T operator()(size_t l, size_t r) const { assert(l < r);
-		T res = t[l+=d];
+		size_t p = l+=d;
 		for(++l,r+=d; l<r; l>>=1,r>>=1) {
-			if(l&1) res = f(res, t[l]), ++l;
-			if(r&1) --r, res = f(res, t[r]);
+			if(l&1) { if(cmp(t[l], t[p])) p = l; ++l; }
+			if(r&1) { --r; if(cmp(t[r], t[p])) p = r; }
 		}
-		return res;
+		return t[p];
 	}
 	const T& operator()() const { return t[1]; }
 	const T& operator[](size_t i) const { return t[i+d]; }
 	private:
 	size_t d;
-	valarray<T> t;
+	vector<T> t;
 };
